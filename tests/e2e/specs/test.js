243f5aac4cc673c8418b4ba9d833fd736ed184c7
chuck driesler <cdriesler.iv@gmail.com>
2019-12-23 23:57:43 -0500

    add ci test command

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:8080/')
     cy.get('.footer__version').should('exist');
   })
 })
