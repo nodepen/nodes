7c30b2b4ff9a68c2905721f62fce3d76c5defa97
chuck driesler <cdriesler.iv@gmail.com>
2020-05-30 11:35:05 -0400

    add server-side svgar

--- server/io/socket.ts
+++ server/io/socket.ts
@@ -1,7 +1,13 @@
 import SocketIO from 'socket.io'
+import * as Svgar from 'svgar-server'
 
 export const rootSocket: (s: SocketIO.Socket) => void = (socket) => {
-  console.log('connection!')
+  const cube = new Svgar.Cube()
+
+  cube.initialize().then(() => {
+    socket.emit('howdy', { message: 'svgar initialized' })
+  })
+
   socket.on('poke', () => {
     console.log('poked!')
     socket.emit('howdy', { message: 'well howdy' })
