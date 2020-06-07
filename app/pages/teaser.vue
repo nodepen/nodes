86d9bb378b05c296bd7e841263ee2275b32f6890
chuck driesler <cdriesler.iv@gmail.com>
2020-06-06 20:10:12 -0400

    (app-teaser) finish first teaser page

--- app/pages/teaser.vue
+++ app/pages/teaser.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="w-vw h-vh bg-green flex flex-col justify-evenly items-center lg:flex-row">
-    <div class="w-48 h-4"></div>
+    <div class="w-48 h-8 arrow">
+      <svg width="192px" height="32" viewBox="0 0 192 32">
+        <line x1="4" y1="16" x2="188" y2="16" stroke="#333333" stroke-width="0.8mm" stroke-linecap="round" />
+        <line x1="188" y1="16" x2="174" y2="2" stroke="#333333" stroke-width="0.8mm" stroke-linecap="round" />
+        <line x1="188" y1="16" x2="174" y2="30" stroke="#333333" stroke-width="0.8mm" stroke-linecap="round" />
+      </svg>
+    </div>
     <div ref="circle" class="w-76 h-76 lg:w-128 lg:h-128 rounded-full bg-pale overflow-hidden flex flex-col justify-center items-center">
       <div ref="svg" :style="backgroundTransform"></div>
-      <div class="card-mono w-76 z-10 p-2 flex flex-col justify-center items-center">
+      <div class="card-mono w-64 lg:w-76 z-10 p-2 flex flex-col justify-center items-center">
         <h1 class="font-display text-3xl mb-2">glasshopper.io</h1>
         <p class="font-sans font-semibold text-sm mb-2">COMING SOON</p>
       </div>
     </div>
     <div @click="navigateToGithub" class="w-48 h-10 card-mono transition-all duration-150 ease-in-out hover:cursor-pointer transform translate-y-0 hover:translate-y-hov-sm flex flex-row">
-      <div class="w-10 border-r border-dark"></div>
+      <div class="w-10 border-r border-dark flex justify-center items-center">
+        <img src="github.svg" width="24px" height="24px"/>
+      </div>
       <div class="flex-grow flex flex-row justify-center items-center">
-        <a href="https://github.com/cdriesler/glasshopper.io" target="_blank" class="font-sans font-semibold text-sm">UPDATES</a>
+        <a href="https://github.com/cdriesler/glasshopper.io" target="_blank" class="font-sans font-semibold text-sm">VIEW UPDATES</a>
       </div>
     </div>
   </div>
@@ -25,7 +33,8 @@ export default Vue.extend({
       w: 0,
       h: 0,
       pct: 0,
-      backgroundTransform: {}
+      backgroundTransform: {},
+      arrowTransform: {}
     }
   },
   mounted() {
@@ -76,5 +85,22 @@ export default Vue.extend({
 <style scoped>
 .card-mono {
   @apply border-2 border-solid border-dark shadow-osm bg-light;
+}
+
+@keyframes arrowloop {
+  from {
+    transform: translateX(-15px);
+  }
+  to {
+    transform: translateX(15px);
+  }
+}
+
+.arrow {
+  animation-name: arrowloop;
+  animation-duration: 800ms;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
 }
 </style>
