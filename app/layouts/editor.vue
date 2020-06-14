08f1a0dc4ad90346a33333ef531bcfaa8c0b48bf
chuck driesler <cdriesler.iv@gmail.com>
2020-06-13 23:18:15 -0400

    (app:meta) technically progress on editor layout

---
+++ app/layouts/editor.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-vw h-vh flex flex-col">
+    <div class="h-32 bg-green">
+      <nuxt />
+    </div>
+    <div class="flex-grow bg-gray-500"></div>
+    <div class="h-16 bg-green"></div>
+  </div>
+</template>
