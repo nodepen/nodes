59de0e01374d0020aba943728694c0439e37ea43
Chuck Driesler <cdriesler.iv@gmail.com>
2020-09-07 21:29:36 -0400

    prepare app for gcloud deploy

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  distDir: 'build',
   async redirects() {
     return [
       {
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,10 +2,32 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 
 import '../styles/tailwind.css'
-import '../styles/fonts.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style global jsx>{`
+        @import url('https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nova+Mono&display=swap');
+
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp
