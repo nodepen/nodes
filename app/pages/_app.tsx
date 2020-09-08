cf4430b954856efc5c2f17efffc336e16db098c9
Chuck Driesler <cdriesler.iv@gmail.com>
2020-09-07 20:28:01 -0400

    translate teaser page to react

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
+
 import '../styles/tailwind.css'
 import '../styles/fonts.css'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps): React.ReactNode => {
   return <Component {...pageProps} />
 }
 
