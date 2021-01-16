7a1c2a9c13164fbe3597032344874332b7310312
Chuck Driesler <cdriesler.iv@gmail.com>
2021-01-15 23:00:11 -0500

    add one test to test coverage

--- app/utils/position/getExtents.spec.ts
+++ app/utils/position/getExtents.spec.ts
@@ -1,9 +1,19 @@
 import { getExtents } from './getExtents'
 
 describe('given an arbitrary region', () => {
-  it('returns correct extents', () => {
-    const [min, max] = getExtents([5, 7], [1, 2])
+  const [min, max] = getExtents([5, 7], [1, 2])
 
-    expect(min[0]).toEqual(1)
+  it('returns correct minimum', () => {
+    const [x, y] = min
+
+    expect(x).toEqual(1)
+    expect(y).toEqual(2)
+  })
+
+  it('returns correct maximum', () => {
+    const [x, y] = max
+
+    expect(x).toEqual(5)
+    expect(y).toEqual(7)
   })
 })
