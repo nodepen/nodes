0c643baa309b5013dc50ac8d71aaa04bb47387ca
Chuck Driesler <cdriesler.iv@gmail.com>
2021-02-13 18:51:15 -0500

    rip panel sidecar-scene

--- app/components/graph/lib/elements/Panel.tsx
+++ app/components/graph/lib/elements/Panel.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import dynamic from 'next/dynamic'
-import { Glasshopper, Grasshopper } from 'glib'
+import { Glasshopper } from 'glib'
 import { useGraphManager } from '@/context/graph'
 import { Grip, DataTree } from './common'
-// import { PanelScene } from './../scene'
 
 type PanelProps = {
   instanceId: string
@@ -53,9 +51,6 @@ export const Panel = ({ instanceId: id }: PanelProps): React.ReactElement => {
   const [dx, dy] = current.position
 
   const label = source ? (elements[source.element].template as any).nickname : 'N/A'
-  const showScene = label === 'Pt'
-
-  const Canvas = dynamic(import('./scene/Panel'), { ssr: false })
 
   return (
     <div className="absolute flex flex-row" style={{ left: dx, top: -dy - 128 }}>
@@ -64,9 +59,7 @@ export const Panel = ({ instanceId: id }: PanelProps): React.ReactElement => {
       </div>
       <div className={`w-8 h-64 bg-light border-2 border-dark rounded-tl-md rounded-bl-md shadow-osm z-30`} />
       <div
-        className={`w-40 h-64 p-2 pt-1 bg-pale border-2 border-green ${
-          showScene ? '' : 'rounded-tr-md rounded-br-md'
-        } overflow-hidden`}
+        className={`w-40 h-64 p-2 pt-1 bg-pale border-2 border-green rounded-tr-md rounded-br-md overflow-hidden`}
         style={{ transform: 'translateY(2px)' }}
       >
         {values ? (
@@ -74,14 +67,6 @@ export const Panel = ({ instanceId: id }: PanelProps): React.ReactElement => {
         ) : (
           <div className="w-full h-full mt-1 rounded-sm border-2 border-dashed border-green" />
         )}
-      </div>
-      <div
-        className="w-64 h-64 bg-pale border-2 border-l-0 border-green rounded-tr-md rounded-br-md"
-        style={{ opacity: showScene && values ? 1 : 0, transform: 'translateY(2px)' }}
-      >
-        {showScene ? (
-          <Canvas points={values && showScene ? Object.values(values)[0].map((v) => v.data as any) : []} />
-        ) : null}
       </div>
     </div>
   )
