1fffbec8f74b45915a8ffdc70f4d64a9d051823a
Chuck Driesler <cdriesler.iv@gmail.com>
2021-02-12 21:27:58 -0500

    fix height on mobile screens

--- app/components/layout/RootLayout.tsx
+++ app/components/layout/RootLayout.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 type RootLayoutProps = {
   children?: React.ReactNode
 }
 
 export const RootLayout = ({ children }: RootLayoutProps): JSX.Element => {
+  const [height, setHeight] = useState<number>()
+
+  useEffect(() => {
+    const h = window.innerHeight
+
+    setHeight(h)
+  }, [])
+
   return (
-    <div className="w-vw h-vh p-0 flex flex-col overflow-hidden">
+    <div className="w-vw p-0 flex flex-col overflow-hidden" style={{ height: height ?? '100vh' }}>
       <div className="w-full h-10 min-h-10 pl-8 pr-8 border-b-2 border-dark bg-light flex flex-row justify-evenly items-center z-50">
         <div className="w-6 overflow-visible flex-grow" />
         <a className="font-display text-lg no-underline select-none" href="/">
