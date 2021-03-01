b9bb58871470fb12fe16d5b48020d60bf0063de6
Chuck Driesler <cdriesler.iv@gmail.com>
2021-02-28 22:08:58 -0500

    show tooltips in library menu and handle enter press

--- app/components/graph/lib/annotation/LibraryCommand.tsx
+++ app/components/graph/lib/annotation/LibraryCommand.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState, useEffect, useRef } from 'react'
 import { Grasshopper } from 'glib'
 import { useGraphManager } from '@/context/graph'
+import { Tooltip } from './Tooltip'
 
 type LibraryCommandProps = {
   position: [number, number]
@@ -51,10 +52,13 @@ export const LibraryCommand = ({ position, onDestroy }: LibraryCommandProps): Re
 
   const [search, setSearch] = useState('')
 
-  const options = [...installed]
-    .sort((a, b) => levDist(search, a.name) - levDist(search, b.name))
-    .slice(0, 5)
-    .reverse()
+  const options =
+    search.length > 0
+      ? [...installed]
+          .sort((a, b) => levDist(search, a.name) - levDist(search, b.name))
+          .slice(0, 5)
+          .reverse()
+      : []
 
   const handlePlaceComponent = (component: Grasshopper.Component): void => {
     if (component.category.toLowerCase() === 'params') {
@@ -78,32 +82,65 @@ export const LibraryCommand = ({ position, onDestroy }: LibraryCommandProps): Re
     onDestroy()
   }
 
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent): void => {
+      if (e.code === 'Enter') {
+        handlePlaceComponent(options[4])
+      }
+    }
+
+    window.addEventListener('keypress', handleKeyPress)
+
+    return () => {
+      window.removeEventListener('keypress', handleKeyPress)
+    }
+  })
+
+  const handlePointerOver = (e: React.PointerEvent<HTMLButtonElement>, component: Grasshopper.Component): void => {
+    const { pageX, pageY } = e
+
+    const tooltip = <Tooltip component={component} onDestroy={() => ''} />
+
+    dispatch({ type: 'tooltip/set-tooltip', position: [pageX + 5, pageY], content: tooltip })
+  }
+
+  const handlePointerLeave = (): void => {
+    dispatch({ type: 'tooltip/clear-tooltip' })
+  }
+
   return (
     <div
       className="w-48 h-64 flex flex-col items-center"
       ref={menuRef}
       style={{ transform: 'translate(-96px, -240px)' }}
     >
-      <div className="w-full flex-grow p-2 bg-pale border-2 border-green rounded-md flex flex-col justify-between">
-        {options.map((option, i) => (
-          <button
-            key={`lib-opt-${option.nickname}-${i}`}
-            onClick={() => handlePlaceComponent(option)}
-            className="w-full h-6 p-1 pr-2 pl-2 mt-1 rounded-sm hover:bg-green flex items-center"
-          >
-            <img
-              width="18px"
-              height="18px"
-              draggable="false"
-              src={`data:image/png;base64,${option.icon}`}
-              alt={option.name}
-              className="mr-4"
-            />
-            <p className="whitespace-no-wrap text-md select-none" style={{ transform: 'translateY(-1px)' }}>
-              {option.name}
-            </p>
-          </button>
-        ))}
+      <div
+        className="w-full flex-grow p-2 bg-pale border-2 border-green rounded-md flex flex-col justify-between"
+        onPointerLeave={handlePointerLeave}
+      >
+        <div className="w-full flex-grow flex flex-col justify-between">
+          {options.map((option, i) => (
+            <button
+              key={`lib-opt-${option.nickname}-${i}`}
+              onClick={() => handlePlaceComponent(option)}
+              className="w-full h-6 p-2 mt-1 rounded-sm hover:bg-green flex items-center"
+              onPointerMove={(e) => handlePointerOver(e, option)}
+              onPointerLeave={handlePointerLeave}
+            >
+              <img
+                width="18px"
+                height="18px"
+                draggable="false"
+                src={`data:image/png;base64,${option.icon}`}
+                alt={option.name}
+                className="mr-4"
+              />
+              <p className="whitespace-no-wrap text-md select-none" style={{ transform: 'translateY(-1px)' }}>
+                {option.name}
+              </p>
+            </button>
+          ))}
+        </div>
         <input
           type="text"
           className="mt-2 mb-1 pl-2 pr-2 rounded-md border-2 border-dark shadow-osm"
