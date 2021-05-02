2cb3c6138a324d1cf499fc0d8e56d334e1ff1c64
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-02 17:22:20 -0600

    abstract configure button to its own component

--- app/components/graph/lib/elements/common/index.ts
+++ app/components/graph/lib/elements/common/index.ts
@@ -1,3 +1,4 @@
+export { ConfigureButton } from './ConfigureButton'
 export { DataTreeContainer as DataTree } from './DataTreeContainer'
 export { DetailsContainer as Details } from './DetailsContainer'
 export { GripContainer as Grip } from './GripContainer'
