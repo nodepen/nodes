2628d507ab4fa1272e91ccb3c37107971250432f
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-02 17:48:32 -0600

    configure parameter with simpler menu

--- app/components/graph/lib/elements/StaticParameter.tsx
+++ app/components/graph/lib/elements/StaticParameter.tsx
@@ -3,10 +3,11 @@ import { Glasshopper } from 'glib'
 import { useGraphManager } from '@/context/graph'
 import { graph } from '@/utils'
 import { ParameterIcon, ParameterIconShadow, ParameterSetValue } from './parameters'
-import { Details, Grip, DataTree, RuntimeMessage, Loading } from './common'
+import { Details, Grip, DataTree, RuntimeMessage, Loading, ConfigureButton } from './common'
 import { useElementStatus, useMoveableElement, useSelectableElement } from './utils'
 import { Tooltip } from '../annotation'
 import { useLongHover } from '@/hooks'
+import { NumberInput } from '~/components/input/NumberInput'
 
 type StaticComponentProps = {
   instanceId: string
@@ -63,7 +64,7 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
 
   const wireIsBlocking = elements['live-wire'] ? (elements['live-wire']?.current as any)?.mode !== 'hidden' : false
 
-  const detailsVisible = !wireIsBlocking && (overPanel || overDetails)
+  const [isConfiguring, setIsConfiguring] = useState(false)
 
   const captureMouseDown = (e: React.MouseEvent<HTMLDivElement>): void => {
     e.stopPropagation()
@@ -82,6 +83,7 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
           component={library[template.category.toLowerCase()][template.subcategory.toLowerCase()].find(
             (t) => t.guid === template.guid
           )}
+          data={current.values}
           runtimeMessage={parameter.current?.runtimeMessage}
           onDestroy={() => dispatch({ type: 'tooltip/clear-tooltip' })}
         />
@@ -93,6 +95,22 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
 
   const longHoverTarget = useLongHover(handleLongHover)
 
+  const [parameterValue, setParameterValue] = useState(0)
+  const [temporaryParameterValue, setTemporaryParameterValue] = useState(0)
+
+  // useEffect(() => {
+  //   const value = Object.values(current.values)?.[0]?.[0] as Glasshopper.Data.DataTreeValue<'number'>
+
+  //   console.log(`Updating value to ${value.data}`)
+
+  //   if (!value) {
+  //     return
+  //   }
+
+  //   setParameterValue(value.data)
+  //   setTemporaryParameterValue(value.data)
+  // }, [current.values])
+
   if (!elements[id] || elements[id].template.type !== 'static-parameter') {
     console.error(`Mismatch with element '${id}' and attempted type 'static-parameter'`)
     return null
@@ -101,6 +119,13 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
   return (
     <div className="absolute flex flex-row justify-center w-48" style={{ left: dx - 96, top: -dy - 18 }}>
       <div className="flex flex-col items-center" ref={parameterRef}>
+        {isConfiguring ? null : (
+          <div className="relative w-0 h-0 overflow-visible">
+            <div className="absolute w-8 h-8" style={{ left: -90, top: 4 }}>
+              <ConfigureButton onClick={() => setIsConfiguring(true)} />
+            </div>
+          </div>
+        )}
         <div
           className="flex flex-row justify-center items-center relative z-20"
           onPointerEnter={() => setHovers(([, details]) => [true, details])}
@@ -137,7 +162,7 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
             <Grip source={{ element: parameter.id, parameter: 'output' }} />
           </div>
         </div>
-        {/* {detailsPinned || detailsVisible ? (
+        {isConfiguring ? (
           <div
             className="flex flex-col w-48 overflow-hidden z-10"
             style={{ transform: 'translate(0, -18px)' }}
@@ -146,10 +171,37 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
           >
             <Details pinned={detailsPinned} onPin={() => setDetailsPinned((current) => !current)}>
               <>
-                {current.runtimeMessage ? (
-                  <RuntimeMessage message={current.runtimeMessage.message} level={current.runtimeMessage.level} />
-                ) : null}
-                {(() => {
+                <NumberInput
+                  value={temporaryParameterValue}
+                  onChange={(value) => setTemporaryParameterValue(Number.parseFloat(value))}
+                />
+                <div className="w-full mt-2 flex flex-row items-center gap-2">
+                  <button
+                    onClick={() => {
+                      setIsConfiguring(false)
+                      setTemporaryParameterValue(parameterValue)
+                    }}
+                    className="p-0 pt-1 flex-grow flex justify-center item-center border-2 border-green rounded-sm font-panel text-sm font-medium text-darkgreen hover:bg-green"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => {
+                      setIsConfiguring(false)
+                      setParameterValue(temporaryParameterValue)
+                      dispatch({
+                        type: 'graph/values/set-one-value',
+                        targetElement: id,
+                        targetParameter: 'input',
+                        value: parameterValue.toString(),
+                      })
+                    }}
+                    className="p-0 pt-1 flex-grow flex justify-center item-center border-2 border-green rounded-sm font-panel text-sm font-medium text-darkgreen hover:bg-green"
+                  >
+                    Submit
+                  </button>
+                </div>
+                {/* {(() => {
                   if (Object.keys(current.values).length > 0) {
                     const valueCount = graph.getValueCount(parameter, 'output')
                     return (
@@ -174,11 +226,11 @@ export const StaticParameter = ({ instanceId: id }: StaticComponentProps): React
                   }
 
                   return <ParameterSetValue element={id} keepOpen={() => setDetailsPinned(true)} />
-                })()}
+                })()} */}
               </>
             </Details>
           </div>
-        ) : null} */}
+        ) : null}
       </div>
     </div>
   )
