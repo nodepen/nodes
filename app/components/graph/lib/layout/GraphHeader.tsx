ddd4ae1192f621bf3bb2c300c94dbd93a154d744
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-07 18:00:33 -0400

    add relevant buttons to new home page

--- app/components/graph/lib/layout/GraphHeader.tsx
+++ app/components/graph/lib/layout/GraphHeader.tsx
@@ -159,6 +159,28 @@ export const GraphHeader = (): React.ReactElement => {
         </svg>
         <p className="ml-2 hidden md:block">Recenter</p>
       </button>
+      <a
+        href="/alpha/scene"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-2 h-6 mr-2 rounded-full border-2 border-swampgreen flex flex-row items-center justify-center font-display text-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+          />
+        </svg>
+        <p className="ml-2 hidden md:block">View Model</p>
+      </a>
       <div className="h-full flex-grow flex flex-row items-center justify-end">
         <span className="hidden md:block">{getMessage(status)}</span>
         {getIcon(status)}
