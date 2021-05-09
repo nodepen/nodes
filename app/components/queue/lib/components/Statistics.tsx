da4c5b1fbe2e9f91e0a904486043065f67e2756b
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-08 23:13:35 -0400

    handle dead letters better

--- app/components/queue/lib/components/Statistics.tsx
+++ app/components/queue/lib/components/Statistics.tsx
@@ -7,18 +7,20 @@ type StatisticsProps = {
 }
 
 export const Statistics = ({ values, title, floor = 0 }: StatisticsProps): React.ReactElement => {
-  const actualAverage = values.map((t) => t - floor).reduce((t, x) => t + x, 0) / values.length
+  const adjustedValues = values.map((v) => v - floor)
+
+  const actualAverage = adjustedValues.map((t) => t - floor).reduce((t, x) => t + x, 0) / adjustedValues.length
   const avg = Math.round(actualAverage)
-  const max = Math.max(...values)
-  const now = values[0]
+  const max = Math.max(...adjustedValues)
+  const now = adjustedValues[0]
 
   return (
     <div className="p-2" style={{ width: 500 }}>
-      {values.map((val, i) => (
+      {adjustedValues.map((val, i) => (
         <div
           key={`${val}-${i}`}
           className="inline-block w-8 h-8 bg-dark mr-2"
-          style={{ background: val == 0 ? '#F7F7F7' : `rgba(0, 0, 0, ${val / max})` }}
+          style={{ background: val === 0 ? '#F7F7F7' : `rgba(0, 0, 0, ${val / max})` }}
         />
       ))}
       <div className="inline-block w-full pr-2 h-8">
@@ -26,8 +28,8 @@ export const Statistics = ({ values, title, floor = 0 }: StatisticsProps): React
           <p className="font-panel text-xl text-dark font-black flex-grow">{title.toUpperCase()}</p>
           {floor > 0 ? <p className="font-panel text-xl text-dark font-black mr-2">({floor})</p> : null}
           <p className="font-panel text-xl text-dark font-black mr-2">+{max}</p>
-          <p className="font-panel text-xl text-dark font-black mr-2">/{avg - floor}</p>
-          <p className="font-panel text-xl text-dark font-black mr-2">&gt;{now - floor}</p>
+          <p className="font-panel text-xl text-dark font-black mr-2">/{avg}</p>
+          <p className="font-panel text-xl text-dark font-black mr-2">&gt;{now}</p>
         </div>
       </div>
     </div>
