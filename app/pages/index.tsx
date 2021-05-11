3dc8fae5ae7c29bfe6b2ed1a463cc857a12faa8e
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-10 22:09:21 -0400

    reset home page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -52,29 +52,19 @@ const Teaser = () => {
     setCircleDimensions([clientWidth, clientHeight])
   }, [])
 
-  // useEffect(() => {
-  //   const speed = 0.25 / 45
-  //   const march = setInterval(() => {
-  //     setOffset((offset + speed) % 0.25)
-  //   }, 40)
-  //   return () => clearInterval(march)
-  // }, [offset])
+  useEffect(() => {
+    const speed = 0.25 / 45
+    const march = setInterval(() => {
+      setOffset((offset + speed) % 0.25)
+    }, 40)
+    return () => clearInterval(march)
+  }, [offset])
 
   return (
     <div className="w-vw h-vh bg-green flex flex-col justify-evenly items-center lg:flex-row">
-      <div className="w-48 h-48 md:w-76 md:h-76 relative">
-        <div className="absolute left-0 top-0 w-48 h-48 md:w-76 md:h-76 rounded-full bg-pale animate-swell z-10" />
-        <div className="absolute left-0 top-0 w-48 h-48 md:w-76 md:h-76 z-20">
-          <div className="flex flex-col items-center justify-center w-full h-full">
-            <a href="/alpha/graph" className="font-sans font-semibold text-sm">
-              <div className="w-48 h-10 card-mono rounded-md transition-all duration-150 ease-in-out hover:cursor-pointer transform translate-y-0 hover:translate-y-hov-sm flex flex-row">
-                <div className="flex-grow flex flex-row justify-center items-center">
-                  <div className="font-sans font-semibold text-sm">LAUNCH NODEPEN</div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
+      <div className="w-76 flex flex-col items-center">
+        <p>1212</p>
+        <p>245</p>
       </div>
       <div
         ref={circleRef}
@@ -92,40 +82,17 @@ const Teaser = () => {
         </div>
         <div className="card-mono rounded-md w-64 lg:w-76 z-10 p-2 flex flex-col justify-center items-center">
           <h1 className="font-display text-3xl mb-2">nodepen</h1>
-          <p className="font-sans font-semibold text-sm mb-2">PUBLIC TEST MAY 7-9</p>
+          <p className="font-sans font-semibold text-sm mb-2">SAME GRASSHOPPER, NEW DIGS</p>
         </div>
       </div>
-      <div className="flex flex-col">
-        <a
-          href="https://github.com/cdriesler/nodepen"
-          target="_blank"
-          rel="noreferrer"
-          className="font-sans font-semibold text-sm mb-2"
-        >
+      <div className="w-76 flex flex-col items-center">
+        <a href="/alpha/graph" className="font-sans font-semibold text-sm">
           <div className="w-48 h-10 card-mono rounded-md transition-all duration-150 ease-in-out hover:cursor-pointer transform translate-y-0 hover:translate-y-hov-sm flex flex-row">
-            <div className="w-10 border-r-2 border-dark flex justify-center items-center">
-              <img alt="The official GitHub logo." src="/github.svg" width="24px" height="24px" />
-            </div>
             <div className="flex-grow flex flex-row justify-center items-center">
-              <div className="font-sans font-semibold text-sm">VIEW SOURCE</div>
+              <div className="font-sans font-semibold text-sm">LAUNCH NODEPEN</div>
             </div>
           </div>
-        </a>
-        <a
-          href="https://twitter.com/cdriesler"
-          target="_blank"
-          rel="noreferrer"
-          className="font-sans font-semibold text-sm"
-        >
-          <div className="w-48 h-10 card-mono rounded-md transition-all duration-150 ease-in-out hover:cursor-pointer transform translate-y-0 hover:translate-y-hov-sm flex flex-row">
-            <div className="w-10 border-r-2 border-dark flex justify-center items-center">
-              <img alt="The official GitHub logo." src="/twitter.svg" width="24px" height="24px" />
-            </div>
-            <div className="flex-grow flex flex-row justify-center items-center">
-              <div className="font-sans font-semibold text-sm">VIEW UPDATES</div>
-            </div>
-          </div>
-        </a>
+        </a>{' '}
       </div>
 
       <style jsx>{`
