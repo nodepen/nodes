6757a8e92d75682dd5ab8499d3aa3541c8c67c01
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-12 00:22:36 -0400

    also sync scale with grid size

--- app-b/pages/index.tsx
+++ app-b/pages/index.tsx
@@ -3,6 +3,7 @@ import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 const Home = (): React.ReactElement => {
   const [[dx, dy], setPosition] = useState<[number, number]>([0, 0])
+  const [scale, setScale] = useState(1)
   const [ready, setReady] = useState(false)
 
   const canvasRef = useRef<HTMLDivElement>(null)
@@ -13,13 +14,38 @@ const Home = (): React.ReactElement => {
 
     const canvas = canvasRef.current
 
-    setPosition([canvas.clientWidth / 2, canvas.clientHeight / 2])
     setReady(true)
   }, [])
 
   const [w, h] = useMemo(
     () => (!ready ? [0, 0] : [canvasRef.current?.clientWidth ?? 0, canvasRef.current?.clientHeight ?? 0]),
     [canvasRef, ready]
+  )
+
+  const c = useMemo(
+    () => (
+      <div className="w-full h-full relative overflow-visible">
+        <TransformWrapper
+          defaultScale={1}
+          options={{ limitToWrapper: false, limitToBounds: false, centerContent: false }}
+          onPanning={(x: any) => {
+            setPosition([x.positionX, x.positionY])
+          }}
+          onZoomChange={(zoom: any) => {
+            setScale(zoom.scale)
+            setPosition([zoom.positionX, zoom.positionY])
+          }}
+        >
+          <></>
+          <TransformComponent>
+            <div style={{ width: w, height: h }}>
+              <div className="w-16 h-16 bg-red-500" />
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+    ),
+    [w, h]
   )
 
   return (
@@ -32,34 +58,19 @@ const Home = (): React.ReactElement => {
             ready ? 'opacity-100' : 'opacity-0'
           } w-full h-full bg-pale z-0 overflow-hidden relative transition-opacity duration-75`}
           style={{
-            backgroundSize: '25px 25px',
-            backgroundPosition: `${dx % 25}px ${dy % 25}px`,
-            backgroundImage:
-              'linear-gradient(to right, #98e2c6 0.3mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 0.3mm, transparent 1px, transparent 10px)',
+            backgroundSize: `${25 * scale}px ${25 * scale}px`,
+            backgroundPosition: `${dx % (25 * scale)}px ${dy % (25 * scale)}px`,
+            backgroundImage: `linear-gradient(to right, #98e2c6 ${
+              0.3 * scale
+            }mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 ${
+              0.3 * scale
+            }mm, transparent 1px, transparent 10px)`,
           }}
           onContextMenu={(e) => {
             e.preventDefault()
           }}
         >
-          {ready ? (
-            <div className="w-full h-full absolute overflow-visible">
-              <TransformWrapper
-                defaultScale={1}
-                defaultPositionX={w / 2}
-                defaultPositionY={h / 2}
-                options={{ limitToWrapper: false, limitToBounds: false, centerContent: false }}
-                onPanning={(x: any) => {
-                  setPosition([x.positionX, x.positionY])
-                }}
-              >
-                <TransformComponent>
-                  <div style={{ width: w, height: h }}>
-                    <div className="w-16 h-16 bg-red-500" />
-                  </div>
-                </TransformComponent>
-              </TransformWrapper>
-            </div>
-          ) : null}
+          {c}
         </div>
 
         {/* <div
