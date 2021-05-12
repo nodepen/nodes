7b4f30aed9d6d6b8d25a85e3dfd3ba455be3fea2
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-12 00:02:48 -0400

    match lib pan with graph bg

--- app-b/pages/index.tsx
+++ app-b/pages/index.tsx
@@ -1,5 +1,79 @@
+import { useState, useLayoutEffect, useEffect, useRef, useMemo } from 'react'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
+
 const Home = (): React.ReactElement => {
-  return <div className="w-vw h-vh bg-red-400" />
+  const [[dx, dy], setPosition] = useState<[number, number]>([0, 0])
+  const [ready, setReady] = useState(false)
+
+  const canvasRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return
+    }
+
+    const canvas = canvasRef.current
+
+    setPosition([canvas.clientWidth / 2, canvas.clientHeight / 2])
+    setReady(true)
+  }, [])
+
+  const [w, h] = useMemo(
+    () => (!ready ? [0, 0] : [canvasRef.current?.clientWidth ?? 0, canvasRef.current?.clientHeight ?? 0]),
+    [canvasRef, ready]
+  )
+
+  return (
+    <div className="w-vw h-vh bg-pale flex flex-col">
+      <div className="w-full h-10 bg-white" />
+      <div className="w-full h-12 bg-green" />
+      <div className="w-full flex-grow bg-pale" ref={canvasRef}>
+        <div
+          className={`${
+            ready ? 'opacity-100' : 'opacity-0'
+          } w-full h-full bg-pale z-0 overflow-hidden relative transition-opacity duration-75`}
+          style={{
+            backgroundSize: '25px 25px',
+            backgroundPosition: `${dx % 25}px ${dy % 25}px`,
+            backgroundImage:
+              'linear-gradient(to right, #98e2c6 0.3mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 0.3mm, transparent 1px, transparent 10px)',
+          }}
+          onContextMenu={(e) => {
+            e.preventDefault()
+          }}
+        >
+          {ready ? (
+            <div className="w-full h-full absolute overflow-visible">
+              <TransformWrapper
+                defaultScale={1}
+                defaultPositionX={w / 2}
+                defaultPositionY={h / 2}
+                options={{ limitToWrapper: false, limitToBounds: false, centerContent: false }}
+                onPanning={(x: any) => {
+                  setPosition([x.positionX, x.positionY])
+                }}
+              >
+                <TransformComponent>
+                  <div style={{ width: w, height: h }}>
+                    <div className="w-16 h-16 bg-red-500" />
+                  </div>
+                </TransformComponent>
+              </TransformWrapper>
+            </div>
+          ) : null}
+        </div>
+
+        {/* <div
+              className="w-full h-full bg-pale z-0 overflow-hidden relative"
+              style={{
+                backgroundSize: '25px 25px',
+                backgroundPosition: `${-dx % 25}px ${-dy % 25}px`,
+                backgroundImage:
+                  'linear-gradient(to right, #98e2c6 0.3mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 0.3mm, transparent 1px, transparent 10px)',
+              }}
+            /> */}
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- app-b/tailwind.config.js
+++ app-b/tailwind.config.js
@@ -1,16 +1,98 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
+    fontFamily: {
+      panel: ['Overpass Mono', 'monospace'],
+      display: ['Nova Mono', 'NovaMono', 'monospace'],
+      sans: ['Barlow Semi Condensed', 'Inter', 'sans-serif'],
+    },
+    scale: {
+      '1000': '10',
+    },
     extend: {
+      screens: {
+        xs: '400px',
+      },
+      boxShadow: {
+        osm: '0 2px 0 0 #333333',
+        omd: '0 3px 0 0 #333333',
+        ism: '0 -2px 0 0 #333333',
+      },
+      colors: {
+        dark: '#333333',
+        light: '#FFFFFF',
+        pale: '#eff2f2',
+        green: '#98E2C6',
+        swampgreen: '#7BBFA5',
+        darkgreen: '#093824',
+        warn: '#FFBE71',
+        error: '#FF7171'
+      },
       height: {
         vw: '100vh',
         vh: '100vh',
       },
+      minHeight: {
+        '10': '2.5rem',
+        '12': '3rem',
+      },
+      minWidth: {
+        '8': '32px',
+        '64': '16rem',
+      },
+      spacing: {
+        'hov-sm': '-2px',
+        '76': '304px',
+        '128': '512px',
+      },
+      width: {
+        vw: '100vw',
+        vh: '100vh',
+      },
+      keyframes: {
+        swell: {
+          '0%, 100%': {
+            transform: 'scale(0.85)',
+            'transform-origin': 'center',
+          },
+          '50%': {
+            transform: 'scale(1)',
+            'transform-origin': 'center',
+          },
+        },
+        swellbig: {
+          '0%, 100%': {
+            transform: 'scale(0.5)',
+            'transform-origin': 'center',
+          },
+          '50%': {
+            transform: 'scale(2)',
+            'transform-origin': 'center',
+          },
+        },
+        scroll: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+      animation: {
+        swell: 'swell 3200ms ease-in-out infinite',
+        scroll: 'scroll 3200ms linear infinite',
+      },
+      transitionProperty: {
+        height: 'height',
+        width: 'width',
+        default: 'opacity, margin, padding, border-radius',
+      },
     },
   },
   variants: {
-    extend: {},
+    cursor: ['responsive, hover'],
   },
   plugins: [],
 }
