14b48a6a92fea2576d77c81e4f1e8ba4c50db24f
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-11 22:28:52 -0400

    fix prettier

--- app-b/pages/_app.tsx
+++ app-b/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from 'next/app'
 
 import 'tailwindcss/tailwind.css'
 
-const NodePen = ({ Component, pageProps }: AppProps) => {
-     return <Component {...pageProps} />
+const NodePen = ({ Component, pageProps }: AppProps): React.ReactElement => {
+  return <Component {...pageProps} />
 }
 
 export default NodePen
--- app-b/pages/index.tsx
+++ app-b/pages/index.tsx
@@ -1,6 +1,5 @@
-const Home = () => {
-    return (<div className="w-vw h-vh bg-red-400" />
-  )
+const Home = (): React.ReactElement => {
+  return <div className="w-vw h-vh bg-red-400" />
 }
 
 export default Home
