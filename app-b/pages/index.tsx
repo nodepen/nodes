798897fb5517a8b25f7d1b19c02e7c0d1d78211e
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-12 21:45:03 -0400

    still broken but adjust grid with zoom

--- app-b/pages/index.tsx
+++ app-b/pages/index.tsx
@@ -40,7 +40,7 @@ const Home = (): React.ReactElement => {
           <></>
           <TransformComponent>
             <div style={{ width: w, height: h }}>
-              <Draggable scale={scale} onStart={(e) => {e.stopPropagation()}} >
+              <Draggable scale={scale} onStart={(e) => {e.stopPropagation()}} disabled={scale < 0.5}>
               <div className="w-16 h-16 bg-red-500" />
               </Draggable>
             </div>
@@ -50,6 +50,8 @@ const Home = (): React.ReactElement => {
     ),
     [w, h, scale]
   )
+
+  const meta = scale < 0.5 ? 5 : 1
 
   return (
     <div className="w-vw h-vh bg-pale flex flex-col">
@@ -61,8 +63,8 @@ const Home = (): React.ReactElement => {
             ready ? 'opacity-100' : 'opacity-0'
           } w-full h-full bg-pale z-0 overflow-hidden relative transition-opacity duration-75`}
           style={{
-            backgroundSize: `${25 * scale}px ${25 * scale}px`,
-            backgroundPosition: `${dx % (25 * scale)}px ${dy % (25 * scale)}px`,
+            backgroundSize: `${25 * scale * meta}px ${25 * scale * meta}px`,
+            backgroundPosition: `${dx % (25 * scale * meta)}px ${dy % (25 * scale * meta)}px`,
             backgroundImage: `linear-gradient(to right, #98e2c6 ${
               0.3 * scale
             }mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 ${
