62b6654695cb986287f260103e9960c2e360381a
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-22 20:47:59 -0400

    fetch installed lib on load

---
+++ api-b/src/data/installed.ts
@@ -0,0 +1,4 @@
+import { Grasshopper } from 'glib'
+import lib from './lib.json'
+
+export const installed = lib as Grasshopper.Component[]
--- api-b/src/gql/resolvers/query/getInstalledComponents.ts
+++ api-b/src/gql/resolvers/query/getInstalledComponents.ts
@@ -1,11 +1,11 @@
 import { Grasshopper } from 'glib'
 import { RequestContext } from '../../types'
+import { allowed } from '../../../data/allowed'
 
 export const getInstalledComponents = (
   parent: never,
   args: never,
   context: RequestContext
 ): Grasshopper.Component[] => {
-  console.log(context.user)
-  return []
+  return allowed
 }
