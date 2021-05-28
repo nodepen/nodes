8c84172528d7db33417754db208ac37432b61911
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-28 01:32:33 -0400

    add bundle analyzer to see wtf is happening (answer: firebase)

--- app-b/next.config.js
+++ app-b/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE })
+
+module.exports = withBundleAnalyzer({
     future: {
         webpack5: true,
     },
     distDir: 'build',
-}
+})
