cad4aae8a301a4abba978e5a187c310a606202ad
Chuck Driesler <cdriesler.iv@gmail.com>
2021-05-30 02:02:33 -0400

    `setCameraPosition` must use onClick events

--- app-b/features/graph/components/canvas/elements/StaticComponent.tsx
+++ app-b/features/graph/components/canvas/elements/StaticComponent.tsx
@@ -41,38 +41,54 @@ const StaticComponent = ({ element }: StaticComponentProps): React.ReactElement 
 
   return (
     <div className="w-full h-full pointer-events-none absolute left-0 top-0">
-      <Draggable
-        scale={scale}
-        position={{ x, y }}
-        onStart={(e) => {
-          e.stopPropagation()
-          setZoomLock(true)
-        }}
-        onStop={(_, e) => {
-          const { x, y } = e
-          moveElement(element.id, [x, y])
-          setZoomLock(false)
-          console.log({ x, y })
-        }}
-        disabled={scale < 0.5 || mode !== 'idle'}
-      >
-        <div
-          className={`${
-            isVisible ? 'opacity-100' : 'opacity-0'
-          } relative flex flex-row items-stretch pointer-events-auto`}
+      <div className="w-full h-full relative">
+        <button
+          className="w-6 h-6 bg-red-500 absolute top-0 pointer-events-auto"
+          style={{ left: x - 36, top: y }}
+          onPointerDown={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+          }}
+          onPointerUp={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+          }}
+          onClick={() => {
+            setCameraPosition(-x, -y)
+          }}
+        />
+        <Draggable
+          scale={scale}
+          position={{ x, y }}
+          onStart={(e) => {
+            e.stopPropagation()
+            setZoomLock(true)
+          }}
+          onStop={(_, e) => {
+            const { x, y } = e
+            moveElement(element.id, [x, y])
+            setZoomLock(false)
+            console.log({ x, y })
+          }}
+          disabled={scale < 0.5 || mode !== 'idle'}
         >
           <div
-            className="absolute w-8 h-4 flex justify-center overflow-visible z-30"
-            style={{ top: '-1.2rem', right: '1rem' }}
+            className={`${
+              isVisible ? 'opacity-100' : 'opacity-0'
+            } relative flex flex-row items-stretch pointer-events-auto`}
           >
-            {/* <Loading visible={status === 'waiting'} /> */}
-          </div>
-          <div
-            id="input-grips-container"
-            className="flex flex-col z-20"
-            style={{ paddingTop: '2px', paddingBottom: '2px' }}
-          >
-            {/* {Object.keys(current.inputs).map((parameterId) => (
+            <div
+              className="absolute w-8 h-4 flex justify-center overflow-visible z-30"
+              style={{ top: '-1.2rem', right: '1rem' }}
+            >
+              {/* <Loading visible={status === 'waiting'} /> */}
+            </div>
+            <div
+              id="input-grips-container"
+              className="flex flex-col z-20"
+              style={{ paddingTop: '2px', paddingBottom: '2px' }}
+            >
+              {/* {Object.keys(current.inputs).map((parameterId) => (
                 <div
                   key={`input-grip-${parameterId}`}
                   className="w-4 flex-grow flex flex-col justify-center"
@@ -81,50 +97,49 @@ const StaticComponent = ({ element }: StaticComponentProps): React.ReactElement 
                   {ready ? <Grip source={{ element: id, parameter: parameterId }} /> : null}
                 </div>
               ))} */}
-          </div>
-          <div
-            id="panel-container"
-            ref={componentRef}
-            className="flex flex-row items-stretch rounded-md border-2 border-dark bg-light shadow-osm z-30"
-          >
-            <div id="inputs-column" className="flex flex-col">
-              {/* {Object.entries(current.inputs).map(([parameterId, i]) => (
+            </div>
+            <div
+              id="panel-container"
+              ref={componentRef}
+              className="flex flex-row items-stretch rounded-md border-2 border-dark bg-light shadow-osm z-30"
+            >
+              <div id="inputs-column" className="flex flex-col">
+                {/* {Object.entries(current.inputs).map(([parameterId, i]) => (
                   <ComponentParameter
                     key={`input-${parameterId}-${i}`}
                     source={{ element: id, parameter: parameterId }}
                     mode="input"
                   />
                 ))} */}
-            </div>
-            <div
-              id="label-column"
-              className="w-10 m-1 p-2 rounded-md border-2 border-dark flex flex-col justify-center items-center transition-colors duration-150"
-              style={{ background: '#FFF' }}
-              onClick={() => setCameraPosition(-x, -y)}
-            >
-              <div
-                className="font-panel text-v font-bold text-sm select-none"
-                style={{ writingMode: 'vertical-lr', textOrientation: 'sideways', transform: 'rotate(180deg)' }}
-              >
-                {template?.nickname.toUpperCase()}
               </div>
-            </div>
-            <div id="outputs-column" className="flex flex-col">
-              {/* {Object.entries(current.outputs).map(([parameterId, i]) => (
+              <div
+                id="label-column"
+                className="w-10 m-1 p-2 rounded-md border-2 border-dark flex flex-col justify-center items-center transition-colors duration-150"
+                style={{ background: '#FFF' }}
+              >
+                <div
+                  className="font-panel text-v font-bold text-sm select-none"
+                  style={{ writingMode: 'vertical-lr', textOrientation: 'sideways', transform: 'rotate(180deg)' }}
+                >
+                  {template?.nickname.toUpperCase()}
+                </div>
+              </div>
+              <div id="outputs-column" className="flex flex-col">
+                {/* {Object.entries(current.outputs).map(([parameterId, i]) => (
                   <ComponentParameter
                     key={`output-${parameterId}-${i}`}
                     source={{ element: id, parameter: parameterId }}
                     mode="output"
                   />
                 ))} */}
+              </div>
             </div>
-          </div>
-          <div
-            id="output-grips-container"
-            className="flex flex-col z-20"
-            style={{ paddingTop: '2px', paddingBottom: '2px' }}
-          >
-            {/* {Object.keys(current.outputs).map((parameterId) => (
+            <div
+              id="output-grips-container"
+              className="flex flex-col z-20"
+              style={{ paddingTop: '2px', paddingBottom: '2px' }}
+            >
+              {/* {Object.keys(current.outputs).map((parameterId) => (
                 <div
                   key={`output-grip-${parameterId}`}
                   className="w-4 flex-grow flex flex-col justify-center"
@@ -133,9 +148,10 @@ const StaticComponent = ({ element }: StaticComponentProps): React.ReactElement 
                   {ready ? <Grip source={{ element: id, parameter: parameterId }} /> : null}
                 </div>
               ))} */}
+            </div>
           </div>
-        </div>
-      </Draggable>
+        </Draggable>
+      </div>
     </div>
   )
 }
