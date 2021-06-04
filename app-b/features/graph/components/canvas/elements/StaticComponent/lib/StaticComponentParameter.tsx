5cee1885513509985723bdae6b1e8bea8424a001
Chuck Driesler <cdriesler.iv@gmail.com>
2021-06-03 20:24:12 -0400

    move direct to component position

--- app-b/features/graph/components/canvas/elements/StaticComponent/lib/StaticComponentParameter.tsx
+++ app-b/features/graph/components/canvas/elements/StaticComponent/lib/StaticComponentParameter.tsx
@@ -1,7 +1,9 @@
 import { NodePen, Grasshopper } from 'glib'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { useSetCameraPosition } from 'features/graph/hooks'
 import { useDebugRender } from '@/hooks'
+import { useCameraStaticPosition, useCameraStaticZoom, useGraphDispatch } from 'features/graph/store/hooks'
+import { screenSpaceToCameraSpace } from 'features/graph/utils'
 
 type StaticComponentParameterProps = {
   parent: NodePen.Element<'static-component'>
@@ -15,14 +17,39 @@ const StaticComponentParameter = ({ parent, template, mode }: StaticComponentPar
 
   useDebugRender(`StaticComponentParameter | ${parent.template.name} | ${name} | ${parameterId}`)
 
+  const { registerElementAnchor } = useGraphDispatch()
+  const cameraZoom = useCameraStaticZoom()
+  const cameraPosition = useCameraStaticPosition()
   const setCameraPosition = useSetCameraPosition()
+
+  const gripRef = useRef<SVGSVGElement>(null)
+
+  useEffect(() => {
+    if (!gripRef.current) {
+      return
+    }
+
+    const { width, height, left, top } = gripRef.current.getBoundingClientRect()
+
+    const [sx, sy] = [left + width / 2, top + height / 2]
+
+    const [x, y] = screenSpaceToCameraSpace(
+      { offset: [0, 48 + 36], position: [sx, sy] },
+      { zoom: cameraZoom, position: cameraPosition }
+    )
+
+    const [ex, ey] = current.position
+    const [dx, dy] = [x - ex, y - ey]
+
+    registerElementAnchor({ elementId, anchorId: parameterId, position: [dx, dy] })
+  }, [])
 
   const grip = useMemo(() => {
     const tx = mode === 'input' ? 'translateX(-9px)' : 'translateX(9px)'
     const d = mode === 'input' ? 'M5,2 a1,1 0 0,0 0,8' : 'M5,10 a1,1 0 0,0 0,-8'
 
     return (
-      <svg className="w-4 h-4 overflow-visible" viewBox="0 0 10 10" style={{ transform: tx }}>
+      <svg ref={gripRef} className="w-4 h-4 overflow-visible" viewBox="0 0 10 10" style={{ transform: tx }}>
         <path d={d} fill="#333" stroke="#333" strokeWidth="2px" vectorEffect="non-scaling-stroke" />
         <circle cx="5" cy="5" r="4" stroke="#333" strokeWidth="2px" vectorEffect="non-scaling-stroke" fill="#FFF" />
       </svg>
@@ -47,16 +74,10 @@ const StaticComponentParameter = ({ parent, template, mode }: StaticComponentPar
   const p = mode === 'input' ? 'pr-4' : 'pl-4'
 
   const handleClick = (): void => {
-    const windowWidth = window.innerWidth
-    const componentWidth = 120
-
-    const dx = mode === 'input' ? windowWidth - 45 : -componentWidth + 45
-    const dy = 20
-
     const [x, y] = current.position
-    console.log(current)
+    const [dx, dy] = current.anchors[parameterId]
 
-    setCameraPosition(x, y, 'C', 20)
+    setCameraPosition(x + dx, y + dy, mode === 'input' ? 'TR' : 'TL', 45)
   }
 
   return (
