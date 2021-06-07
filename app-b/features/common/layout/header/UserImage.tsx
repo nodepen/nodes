222d2984d62c95de21f372e3956ddba274ba5387
Chuck Driesler <cdriesler.iv@gmail.com>
2021-06-07 00:53:20 -0400

    small profile pic fix

--- app-b/features/common/layout/header/UserImage.tsx
+++ app-b/features/common/layout/header/UserImage.tsx
@@ -3,5 +3,10 @@ import { useSessionManager } from 'context/session'
 export const UserImage = (): React.ReactElement => {
   const { user } = useSessionManager()
 
-  return <img src={user?.photoURL ?? undefined} style={{ filter: 'grayscale(1)', WebkitFilter: 'grayscale(1)' }} />
+  return (
+    <img
+      src={user?.photoURL ?? undefined}
+      style={{ filter: 'grayscale(1)', WebkitFilter: 'grayscale(1)', width: '100%', height: '100%' }}
+    />
+  )
 }
