d74a18e6c5f8c5dd6841c6038f1fd2775d2e9b4b
Chuck Driesler <cdriesler.iv@gmail.com>
2021-07-03 16:20:11 -0400

    draggable generic menu

--- app-b/features/graph/components/overlay/OverlayContainer.tsx
+++ app-b/features/graph/components/overlay/OverlayContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 type OverlayContainerProps = {
   children: JSX.Element
@@ -9,10 +10,30 @@ export const OverlayContainer = ({ children, position }: OverlayContainerProps):
   const [left, top] = position
 
   return (
-    <div className="w-full h-full pointer-events-none relative">
-      <div className="w-full h-full absolute" style={{ left, top }}>
-        <div className="w-full h-full relative">{children}</div>
-      </div>
+    <div className="w-full h-full relative pointer-events-auto">
+      <TransformWrapper
+        initialScale={1}
+        initialPositionX={left}
+        initialPositionY={top}
+        minScale={1}
+        maxScale={1}
+        limitToBounds={false}
+        wheel={{
+          disabled: true,
+        }}
+        pinch={{
+          disabled: true,
+        }}
+        doubleClick={{
+          disabled: true,
+        }}
+      >
+        <TransformComponent>
+          <div className="w-vw h-vh">
+            <div className="w-full h-full relative">{children}</div>
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   )
 }
