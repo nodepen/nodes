14f668533dd4e6fb470f8b2e4d99a1fb4a438087
Chuck Driesler <cdriesler.iv@gmail.com>
2021-07-10 12:50:55 -0400

    capture second-taps and lock selection region while moving on desktop

--- app-b/features/graph/components/canvas/elements/Region/SelectionRegion.tsx
+++ app-b/features/graph/components/canvas/elements/Region/SelectionRegion.tsx
@@ -28,9 +28,40 @@ const SelectionRegion = ({ region }: SelectionRegionProps): React.ReactElement =
   const { deleteElement, updateLiveElement } = useGraphDispatch()
 
   const regionRef = useRef<HTMLDivElement>(null)
+  const lockRegion = useRef(false)
+
+  const handleMouseDown = useCallback((e: MouseEvent): void => {
+    switch (e.button) {
+      case 2: {
+        lockRegion.current = true
+      }
+    }
+  }, [])
+
+  const handleMouseUp = useCallback((e: MouseEvent): void => {
+    switch (e.button) {
+      case 2: {
+        lockRegion.current = false
+      }
+    }
+  }, [])
+
+  const handlePointerDown = useCallback(
+    (e: PointerEvent | React.PointerEvent<HTMLDivElement>): void => {
+      if (e.pointerId !== pointer) {
+        alert('second touch!')
+        return
+      }
+    },
+    [pointer]
+  )
 
   const handlePointerMove = useCallback(
     (e: PointerEvent | React.PointerEvent<HTMLDivElement>): void => {
+      if (lockRegion.current) {
+        return
+      }
+
       const [cx, cy] = cameraPosition
       const { pageX: ex, pageY: ey } = e
       const [x, y] = screenSpaceToCameraSpace(
@@ -47,45 +78,58 @@ const SelectionRegion = ({ region }: SelectionRegionProps): React.ReactElement =
     [cameraPosition, cameraZoom, region.id, updateLiveElement]
   )
 
-  const handlePointerUp = useCallback((): void => {
-    // Do selection
-    // alert(`from: [${fromX}, ${fromY}] | to: [${toX}, ${toY}]`)
-    setMode('idle')
-    deleteElement(region.id)
-  }, [setMode, deleteElement, region.id, fromX, fromY, toX, toY])
+  const handlePointerUp = useCallback(
+    (e: PointerEvent | React.PointerEvent<HTMLDivElement>): void => {
+      if (e.pointerType === 'mouse' && e.button === 2) {
+        lockRegion.current = false
+        return
+      }
+
+      if (e.pointerId !== pointer) {
+        return
+      }
+      // Do selection
+      // alert(`from: [${fromX}, ${fromY}] | to: [${toX}, ${toY}]`)
+      setMode('idle')
+      deleteElement(region.id)
+    },
+    [setMode, deleteElement, region.id, pointer, fromX, fromY, toX, toY]
+  )
 
   const [fallbackToWindow, setFallbackToWindow] = useState(false)
 
   useEffect(() => {
-    if (!fallbackToWindow) {
-      return
-    }
-
+    window.addEventListener('mousedown', handleMouseDown)
+    window.addEventListener('mouseup', handleMouseUp)
+    window.addEventListener('pointerdown', handlePointerDown)
     window.addEventListener('pointermove', handlePointerMove)
     window.addEventListener('pointerup', handlePointerUp)
 
     return () => {
+      window.removeEventListener('mousedown', handleMouseDown)
+      window.removeEventListener('mouseup', handleMouseUp)
+      window.addEventListener('pointerdown', handlePointerDown)
       window.removeEventListener('pointermove', handlePointerMove)
       window.removeEventListener('pointerup', handlePointerUp)
     }
   })
 
-  useEffect(() => {
-    if (device.iOS) {
-      setFallbackToWindow(true)
-      return
-    }
+  // useEffect(() => {
+  //   if (device.iOS) {
+  //     setFallbackToWindow(true)
+  //     return
+  //   }
 
-    if (!regionRef.current) {
-      return
-    }
+  //   if (!regionRef.current) {
+  //     return
+  //   }
 
-    try {
-      regionRef.current.setPointerCapture(pointer)
-    } catch {
-      setFallbackToWindow(true)
-    }
-  }, [])
+  //   try {
+  //     regionRef.current.setPointerCapture(pointer)
+  //   } catch {
+  //     setFallbackToWindow(true)
+  //   }
+  // }, [])
 
   // Watch pointer motion from here
   const [min, max] = [
@@ -96,9 +140,9 @@ const SelectionRegion = ({ region }: SelectionRegionProps): React.ReactElement =
   return (
     <div
       ref={regionRef}
-      onGotPointerCapture={(e) => e.preventDefault()}
-      onPointerMove={handlePointerMove}
-      onPointerUp={handlePointerUp}
+      // onGotPointerCapture={(e) => e.preventDefault()}
+      // onPointerMove={handlePointerMove}
+      // onPointerUp={handlePointerUp}
       className="absolute z-20"
       style={{ left: min.x, top: min.y, width: Math.abs(max.x - min.x), height: Math.abs(max.y - min.y) }}
     >
