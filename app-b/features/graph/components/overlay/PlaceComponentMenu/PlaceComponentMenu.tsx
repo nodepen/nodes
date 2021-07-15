36d5f70880ecb1e6e189653b340bff3fcf7e9c56
Chuck Driesler <cdriesler.iv@gmail.com>
2021-07-14 23:58:08 -0400

    place component menu text comparison & search behavior

--- app-b/features/graph/components/overlay/PlaceComponentMenu/PlaceComponentMenu.tsx
+++ app-b/features/graph/components/overlay/PlaceComponentMenu/PlaceComponentMenu.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { useGraphDispatch } from 'features/graph/store/graph/hooks'
 import { OverlayPortal } from '../OverlayPortal'
 import { OverlayContainer } from '../OverlayContainer'
 import { useGraphManager } from 'context/graph'
 import { useOverlayOffset } from '../hooks'
+import { levenshteinDistance } from './utils'
 
 type PlaceComponentMenuProps = {
   /** Position to place element in screen coordinate space. */
@@ -17,17 +18,60 @@ export const PlaceComponentMenu = ({ position: screenPosition }: PlaceComponentM
 
   const position = useOverlayOffset(screenPosition)
 
+  const [userValue, setUserValue] = useState<string>('')
+
+  const libraryNames = useMemo(() => {
+    return library?.map((component) => component.name) ?? []
+  }, [library])
+
+  const sortedCandidates = useMemo(() => {
+    if (!userValue || userValue.length <= 0) {
+      return []
+    }
+
+    const candidates = [...libraryNames]
+
+    return candidates.sort((a, b) => levenshteinDistance(a, userValue) - levenshteinDistance(b, userValue))
+  }, [libraryNames, userValue])
+
+  const exactMatch = sortedCandidates.find((candidate) => candidate.toLowerCase().indexOf(userValue.toLowerCase()) == 0)
+
+  if (exactMatch) {
+    sortedCandidates.splice(
+      sortedCandidates.findIndex((candidate) => candidate === exactMatch),
+      1
+    )
+  }
+
+  const displayCandidates = (exactMatch ? [exactMatch, ...sortedCandidates] : sortedCandidates).slice(0, 15)
+  const displayExactMatch = exactMatch ? `${userValue}${exactMatch.substr(userValue.length)}` : ''
+
   return (
     <OverlayPortal>
       <OverlayContainer static position={[0, 0]}>
-        <div className="w-full h-full flex flex-col bg-green">
+        <div className="w-full h-full flex flex-col bg-green pointer-events-auto">
           <div className="w-full h-12 p-2">
-            <div className="w-full h-full flex items-center p-1 pl-2 pr-2 rounded-md bg-pale">
-              <div className="w-6 h-6 mr-2 rounded-sm bg-swampgreen" />
+            <div className="w-full h-full relative rounded-md bg-pale overflow-hidden">
+              <input
+                className="absolute w-full h-full bg-transparent left-0 top-0 z-50"
+                value={userValue}
+                onChange={(e) => setUserValue(e.target.value)}
+              />
+              <input
+                value={displayExactMatch}
+                className="absolute w-full h-full left-0 top-0 z-40 text-swampgreen"
+                disabled
+              />
+              {/* <div className="w-6 h-6 mr-2 rounded-sm bg-swampgreen" />
               <input className="h-6 flex-grow rounded-sm bg-pale" />
               <div className="w-6 h-6 ml-2 rounded-full bg-green" />
-              <div className="w-6 h-6 ml-2 rounded-full bg-green" />
+              <div className="w-6 h-6 ml-2 rounded-full bg-green" /> */}
             </div>
+          </div>
+          <div className="w-full flex flex-col">
+            {displayCandidates.map((name, i) => (
+              <p key={`sort-option-${name}-${i}`}>{name}</p>
+            ))}
           </div>
         </div>
       </OverlayContainer>
---
+++ app-b/features/graph/components/overlay/PlaceComponentMenu/utils/index.ts
@@ -0,0 +1,1 @@
+export { levenshteinDistance } from './levenshteinDistance'
