4276af62c9ed0f72971fbc141272998050d13e7a
Chuck Driesler <cdriesler.iv@gmail.com>
2021-09-08 01:01:20 -0400

    comment

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useRef } from 'react'
 import { NextPage } from 'next'
-import Link from 'next/link'
+// import Link from 'next/link'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
