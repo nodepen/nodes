e49d3349a2523acf4b30fa28b40462adf5e051b2
Chuck Driesler <cdriesler.iv@gmail.com>
2021-09-08 23:10:26 -0400

    typo ):

--- api/src/bq/ghq/ghq.ts
+++ api/src/bq/ghq/ghq.ts
@@ -1,9 +1,9 @@
 import Queue from 'bee-queue'
 import { ClientOpts } from 'redis'
 
-const opts: ClientOpts = process.env?.NP_DP_HOST
+const opts: ClientOpts = process.env.NP_DB_HOST
   ? {
-      host: process.env?.NP_DB_HOST,
+      host: process.env.NP_DB_HOST,
       port: Number.parseInt(process.env?.NP_DB_PORT),
     }
   : {}
