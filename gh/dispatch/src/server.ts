f6bf9987a62a04b266323aeb7fd8142bb0a47232
Chuck Driesler <cdriesler.iv@gmail.com>
2021-09-10 00:11:13 -0400

    expose a healthcheck on dispatch

--- gh/dispatch/src/server.ts
+++ gh/dispatch/src/server.ts
@@ -1,9 +1,22 @@
 import dotenv from 'dotenv'
 import { db } from './db'
+import express from 'express'
 
 dotenv.config()
 
 console.log('howdy!')
+
+const port = 9800
+
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send('Ready to run')
+})
+
+app.listen(port, () => {
+  console.log(`Dispatch listening on port ${port} !`)
+})
 
 // db.client.on('ready', () => {
 //   console.log('Redis connection ready!')
