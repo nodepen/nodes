9b84669841148b2138e69c420ca23224683774d4
Chuck Driesler <cdriesler.iv@gmail.com>
2021-10-27 21:02:05 -0400

    surface correct value strings

--- app/features/graph/components/canvas/common/DataTreePreview.tsx
+++ app/features/graph/components/canvas/common/DataTreePreview.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NodePen } from 'glib'
 import { Typography } from 'features/common'
-import { flattenDataTree } from '@/features/graph/utils'
+import { flattenDataTree, getDataTreePreviewStrings } from '@/features/graph/utils'
 
 type DataTreePreviewProps = {
   elementId: string
@@ -17,33 +17,19 @@ export const DataTreePreview = ({
   details,
 }: DataTreePreviewProps): React.ReactElement => {
   const prefix = `${elementId}-${parameterId}-data-`
-  const values = flattenDataTree(tree ?? {})
+
+  const entries = getDataTreePreviewStrings(tree ?? {})
 
   return (
     <div className="w-full p-2 flex flex-col justify-start items-center">
       <Typography.Data size="sm" color="dark">
         {details}
       </Typography.Data>
-      {values.map(({ value }, i) => (
-        <Typography.Data key={`${prefix}${i}`} size="sm" color="dark">
-          {value.toString()}
+      {entries.map((entry, i) => (
+        <Typography.Data key={`${prefix}-value-${i}`} size="sm" color="dark">
+          {entry}
         </Typography.Data>
       ))}
-      {/* {Object.entries(tree ?? {}).map(([path, values]) => (
-        <div key={`${prefix}`} className="w-full ">
-          <div key={`${prefix}${path}`} className="w-full h-8 flex items-center justify-end">
-            {path}
-          </div>
-          {values.map((value, i) => (
-            <div key={`${prefix}${path}-value-${i}`} className="w-full h-8 flex items-center">
-              <div className="w-8 h-8 flex justify-center items-center">{i}</div>
-              <div className="h-8 flex-grow flex justify-end items-center overflow-hidden whitespace-nowrap">
-                {value.value}
-              </div>
-            </div>
-          ))}
-        </div>
-      ))} */}
     </div>
   )
 }
