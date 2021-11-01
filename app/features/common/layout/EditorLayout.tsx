d8214ee74b2920fb6d83801707ada4875350addd
Chuck Driesler <cdriesler.iv@gmail.com>
2021-10-31 20:01:41 -0400

    style download button

--- app/features/common/layout/EditorLayout.tsx
+++ app/features/common/layout/EditorLayout.tsx
@@ -1,4 +1,4 @@
-// import { useState, useRef, useCallback } from 'react'
+import { useState } from 'react'
 import { useGraphId } from '@/features/graph/store/graph/hooks'
 import { useSolutionMetadata } from '@/features/graph/store/solution/hooks'
 import { useApolloClient, gql } from '@apollo/client'
@@ -32,7 +32,15 @@ export const EditorLayout = ({ children }: EditorLayoutProps): React.ReactElemen
   const graphId = useGraphId()
   const { id: solutionId } = useSolutionMetadata()
 
+  const [isDownloading, setIsDownloading] = useState(false)
+
   const handleDownload = (): void => {
+    if (isDownloading) {
+      return
+    }
+
+    setIsDownloading(true)
+
     client
       .query({
         query: gql`
@@ -51,8 +59,6 @@ export const EditorLayout = ({ children }: EditorLayoutProps): React.ReactElemen
       })
       .then((res) => {
         const { gh } = res.data.solution.files
-
-        console.log({ gh })
 
         if (gh) {
           let fileData: any = atob(gh)
@@ -73,6 +79,12 @@ export const EditorLayout = ({ children }: EditorLayoutProps): React.ReactElemen
 
           URL.revokeObjectURL(objectURL)
         }
+      })
+      .catch((err) => {
+        console.error('🐍 Failed to download grasshopper file for current solution.')
+      })
+      .finally(() => {
+        setIsDownloading(false)
       })
   }
 
@@ -107,7 +119,13 @@ export const EditorLayout = ({ children }: EditorLayoutProps): React.ReactElemen
             className="h-6 w-6 mr-2 border-2 border-dark rounded-sm bg-white flex items-center justify-center"
             onClick={handleDownload}
           >
-            ?
+            <svg className="w-4 h-4" fill="#333333" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
           </button>
           <button
             className={`${isAnonymous ? 'border-2 border-dark' : ''} h-6 w-6 rounded-sm bg-white overflow-hidden`}
