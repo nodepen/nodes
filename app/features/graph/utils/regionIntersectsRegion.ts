d9b84258c17888c0ee4e7d81e8bdc6e5648aa6c0
Chuck Driesler <cdriesler.iv@gmail.com>
2021-11-01 20:57:01 -0400

    repair region selection

--- app/features/graph/utils/regionIntersectsRegion.ts
+++ app/features/graph/utils/regionIntersectsRegion.ts
@@ -9,28 +9,44 @@ export const regionIntersectsRegion = (
   const outer = regionExtents(container[0], container[1])
   const inner = regionExtents(target[0], target[1])
 
-  const tests = [
+  const intersectionTests = [
     domainIntersects([outer.min.x, outer.max.x], [inner.min.x, inner.max.x]),
     domainIntersects([outer.min.y, outer.max.y], [inner.min.y, inner.max.y]),
   ]
 
   // Short-circuit if trivial case detected
-  if (tests.every((test) => test)) {
+  if (intersectionTests.every((test) => test)) {
     return true
   }
 
-  if (tests.every((test) => !test)) {
-    return false
-  }
+  const containmentTests = [
+    domainContains([outer.min.x, outer.max.x], [inner.min.x, inner.max.x]),
+    domainContains([outer.min.y, outer.max.y], [inner.min.y, inner.max.y]),
+    domainContains([inner.min.x, inner.max.x], [outer.min.x, outer.max.x]),
+    domainContains([inner.min.y, inner.max.y], [outer.min.y, outer.max.y]),
+  ]
 
-  // If only one domain intersects, the other must be contained by the region
-  const [xIntersects, yIntersects] = tests
+  const [xIntersects, yIntersects] = intersectionTests
+  const [xContainsTarget, yContainsTarget, xWithinTarget, yWithinTarget] = containmentTests
 
-  if (xIntersects && domainContains([outer.min.y, outer.max.y], [inner.min.y, inner.max.y])) {
+  // If only one domain intersects, the other must be contained by the region or contain the region
+  if (xIntersects && (yWithinTarget || yContainsTarget)) {
     return true
   }
 
-  if (yIntersects && domainContains([outer.min.x, outer.max.x], [inner.min.x, inner.max.x])) {
+  if (yIntersects && (xWithinTarget || xContainsTarget)) {
+    return true
+  }
+
+  // console.log({ xIntersects, yIntersects })
+  // console.log({ xContainsTarget, yContainsTarget, xWithinTarget, yWithinTarget })
+
+  // If only one domain contains the target, then the other domain must be contained by the target
+  if (xContainsTarget && yWithinTarget) {
+    return true
+  }
+
+  if (yContainsTarget && xWithinTarget) {
     return true
   }
 
