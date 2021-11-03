00c997ecc6b82a74ac76a2e094788e44d325065b
Chuck Driesler <cdriesler.iv@gmail.com>
2021-11-02 23:27:59 -0400

    on-concept modal background

--- app/features/common/layout/ModalLayout.tsx
+++ app/features/common/layout/ModalLayout.tsx
@@ -9,25 +9,66 @@ type ModalLayoutProps = {
 export const ModalLayout = ({ children, onClose }: ModalLayoutProps): React.ReactElement => {
   // const { setZoomLock }
   return (
-    <ModalPortal>
-      <div
-        className="relative w-full h-full "
-        // onScroll={(e) => e.stopPropagation()}
-      >
-        <button
-          className="absolute w-full h-full bg-dark opacity-50 left-0 top-0 z-10 pointer-events-auto"
-          onClick={onClose}
-        />
-        <div className="absolute w-full h-full p-10 flex justify-center items-center left-0 top-0 z-20 pointer-events-none">
-          <div
-            className="w-full h-full flex flex-col p-4 bg-white rounded-md pointer-events-auto"
-            style={{ maxWidth: '750px', maxHeight: '500px' }}
-          >
-            {children}
+    <>
+      <ModalPortal>
+        <div
+          className="w-full h-full overflow-hidden"
+          // onScroll={(e) => e.stopPropagation()}
+        >
+          <div className="relative w-full h-full animate-appear">
+            <button
+              className="absolute w-full h-full left-0 top-0 animate-modal z-10 pointer-events-auto bg-none"
+              style={{
+                backgroundSize: `15mm 15mm`,
+                backgroundImage: `linear-gradient(to right, #98e2c6 5mm, transparent 1px, transparent 10px), linear-gradient(to bottom, #98e2c6 5mm, transparent 1px, transparent 10px)`,
+              }}
+              onClick={onClose}
+            />
+            <div className="absolute w-full h-full p-10 flex justify-center items-center left-0 top-0 z-20 pointer-events-none">
+              <div
+                className="w-full h-full flex flex-col p-4 bg-white rounded-md pointer-events-auto"
+                style={{ maxWidth: '750px', maxHeight: '500px' }}
+              >
+                {children}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </ModalPortal>
+      </ModalPortal>
+      <style jsx>{`
+        @keyframes march {
+          from {
+            background-position: left bottom;
+          }
+          to {
+            background-position: right top;
+          }
+        }
+
+        .animate-modal {
+          animation-name: march;
+          animation-duration: 120s;
+          animation-timing-function: linear;
+          animation-iteration-count: infinite;
+        }
+
+        @keyframes appear {
+          from {
+            transform: translateY(-100%);
+          }
+          to {
+            transform: translateY(0);
+          }
+        }
+
+        .animate-appear {
+          animation-name: appear;
+          animation-duration: 300ms;
+          animation-timing-function: ease-out;
+          animation-fill-mode: forwards;
+        }
+      `}</style>
+    </>
   )
 }
 
