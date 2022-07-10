ef0e4fa12d317d11174b71e079635bfda76df93f
Chuck Driesler <cdriesler.iv@gmail.com>
2022-07-09 20:51:56 -0400

    document nodes hook

--- packages/nodes/src/components/nodes/NodesContainer.tsx
+++ packages/nodes/src/components/nodes/NodesContainer.tsx
@@ -1,26 +1,45 @@
 import React from 'react'
+import type * as NodePen from '@nodepen/core'
 import { useStore } from '$'
+
 import { GenericNode } from './generic-node'
 
 const NodesContainer = (): React.ReactElement => {
   // TODO: Pull from state in a smart way
-  const elements = useStore((store) => store.document.elements)
+  const nodes = useDocumentNodes()
 
   return (
     <g id="np-nodes" className="np-pointer-events-auto">
-      {Object.entries(elements).map(([elementId, element], i) => {
+      {nodes.map((node) => {
         // Handle special templates (number slider, panel, etc)
-        const { template } = element
+        const { id, template } = node
 
         switch (template) {
           case 'some-id':
             return null
           default:
-            return <GenericNode id={elementId} />
+            return <GenericNode key={`generic-node-${id}`} id={id} />
         }
       })}
     </g>
   )
+}
+
+const useDocumentNodes = (): NodePen.Element[] => {
+  const nodes = useStore<NodePen.Document['elements']>(
+    (store) => store.document.elements,
+    (previousNodes, currentNodes) => {
+      const previousNodeIds = Object.keys(previousNodes)
+      const currentNodeIds = Object.keys(currentNodes)
+
+      const isSameLength = () => previousNodeIds.length === currentNodeIds.length
+      const isSameValues = () => previousNodeIds.every((id) => currentNodeIds.includes(id))
+
+      return isSameLength() && isSameValues()
+    }
+  )
+
+  return Object.values(nodes)
 }
 
 export default React.memo(NodesContainer)
