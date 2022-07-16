bd7dc5819a14cf807799a0ab5a73ca5edcfc3393
Chuck Driesler <cdriesler.iv@gmail.com>
2022-07-16 01:46:51 -0400

    always a pleasure

---
+++ packages/converters/grasshopper/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using X = Grasshopper;
+
+namespace NodePen.Converters.Grasshopper
+{
+
+    public class Class1
+    {
+        public static void Okay()
+        {
+            Console.WriteLine("OK");
+            // Console.WriteLine(X.Instances.ComponentServer.Libraries.Count);
+        }
+
+    }
+
+}
+
