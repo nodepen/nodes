07af92be4b44e0a6b73015796035c9748574b3df
Chuck Driesler <cdriesler.iv@gmail.com>
2022-07-16 22:55:22 -0400

    just keep the dotnet stuff simple for now

---
+++ packages/converters/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NodePen.Converters
+{
+
+    public class Class1
+    {
+
+        public void Test()
+        {
+            Console.WriteLine("OK");
+        }
+
+    }
+
+}
+
