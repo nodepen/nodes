53ff27398bece6267f3d032d203ea2f0c1eea164
Chuck Driesler <cdriesler.iv@gmail.com>
2022-10-16 11:46:53 -0400

    add and tune label text

--- packages/nodes/src/components/nodes/generic-node/GenericNode.tsx
+++ packages/nodes/src/components/nodes/generic-node/GenericNode.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import type * as NodePen from '@nodepen/core'
 import { useStore } from '$'
 import { COLORS, DIMENSIONS } from '@/constants'
@@ -10,11 +10,14 @@ type GenericNodeProps = {
 }
 
 const {
-  COMPONENT_INTERNAL_PADDING,
-  COMPONENT_LABEL_WIDTH,
-  COMPONENT_MINIMUM_HEIGHT,
-  COMPONENT_PARAMETER_MINIMUM_HEIGHT,
-  COMPONENT_PARAMETER_MINIMUM_WIDTH,
+  NODE_INTERNAL_PADDING,
+  NODE_LABEL_WIDTH,
+  NODE_LABEL_FONT_SIZE,
+  NODE_MINIMUM_HEIGHT,
+  NODE_PORT_LABEL_FONT_SIZE,
+  NODE_PORT_MINIMUM_HEIGHT,
+  NODE_PORT_MINIMUM_WIDTH,
+  NODE_PORT_RADIUS,
 } = DIMENSIONS
 
 const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => {
@@ -27,11 +30,11 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
 
   console.log(`⚙️⚙️⚙️ generic-node : ${id.split('-')[0]} : ${nickName.toLowerCase()}`)
 
-  const nodeWidth = COMPONENT_INTERNAL_PADDING * 4 + COMPONENT_PARAMETER_MINIMUM_WIDTH * 2 + COMPONENT_LABEL_WIDTH
+  const nodeWidth = NODE_INTERNAL_PADDING * 4 + NODE_PORT_MINIMUM_WIDTH * 2 + NODE_LABEL_WIDTH
 
-  const nodeParameterHeight = Math.max(inputs.length, outputs.length, 1) * COMPONENT_PARAMETER_MINIMUM_HEIGHT
-  const nodeContentHeight = Math.max(nodeParameterHeight, COMPONENT_MINIMUM_HEIGHT)
-  const nodeHeight = COMPONENT_INTERNAL_PADDING * 2 + nodeContentHeight
+  const nodeParameterHeight = Math.max(inputs.length, outputs.length, 1) * NODE_PORT_MINIMUM_HEIGHT
+  const nodeContentHeight = Math.max(nodeParameterHeight, NODE_MINIMUM_HEIGHT)
+  const nodeHeight = NODE_INTERNAL_PADDING * 2 + nodeContentHeight
 
   const getInputColumnPosition = (orderNumber: number): [x: number, y: number] => {
     const x = position.x
@@ -39,7 +42,7 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
     const inputColumnVerticalStep = inputs.length === 0 ? nodeContentHeight : nodeContentHeight / inputs.length
 
     const currentVerticalNodePosition = -position.y
-    const currentVerticalPadding = COMPONENT_INTERNAL_PADDING
+    const currentVerticalPadding = NODE_INTERNAL_PADDING
     const currentVerticalPortPosition = orderNumber * inputColumnVerticalStep
     const currentVerticalPortCenterOffset = inputColumnVerticalStep / 2
 
@@ -57,13 +60,13 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
 
     return (
       <circle
-        r={6}
+        r={NODE_PORT_RADIUS}
         cx={x}
         cy={y}
         fill={COLORS.LIGHT}
         stroke={COLORS.DARK}
         strokeWidth={2}
-        vectorEffect="non-scaling-stroke"
+        // vectorEffect="non-scaling-stroke"
       />
     )
   })
@@ -73,20 +76,52 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
 
     return (
       <circle
-        r={6}
+        r={NODE_PORT_RADIUS}
         cx={x}
         cy={y + 2}
         fill={COLORS.DARK}
         stroke={COLORS.DARK}
         strokeWidth={2}
-        vectorEffect="non-scaling-stroke"
+        // vectorEffect="non-scaling-stroke"
       />
     )
   })
 
-  // const inputPortLabels = inputs.map((input, i) => (
-  //   <
-  // ))
+  const inputPortLabels = inputs.map((input, i) => {
+    const { nickName } = input
+
+    const [x, y] = getInputColumnPosition(i)
+
+    return (
+      <text
+        x={x + 12}
+        y={y - 3 + NODE_PORT_LABEL_FONT_SIZE / 2}
+        className="np-font-mono np-select-none"
+        fontSize={NODE_PORT_LABEL_FONT_SIZE}
+        fill={COLORS.DARK}
+      >
+        {nickName}
+      </text>
+    )
+  })
+
+  const nodeLabel = (
+    <>
+      <path
+        id={`node-label-${id}`}
+        fill="none"
+        // stroke="red"
+        d={`M ${position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3} ${-position.y + nodeHeight} L ${
+          position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3
+        } ${-position.y}`}
+      />
+      <text className="np-font-panel np-select-none" fill={COLORS.DARK} fontSize={NODE_LABEL_FONT_SIZE}>
+        <textPath href={`#node-label-${id}`} startOffset="50%" textAnchor="middle" alignmentBaseline="middle">
+          {nickName.toUpperCase()}
+        </textPath>
+      </text>
+    </>
+  )
 
   return (
     <g id={`generic-node-${id}`} ref={draggableTargetRef}>
@@ -101,7 +136,7 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
         fill={COLORS.DARK}
         stroke={COLORS.DARK}
         strokeWidth={2}
-        vectorEffect="non-scaling-stroke"
+        // vectorEffect="non-scaling-stroke"
       />
       {inputPortShadows}
       {/* Body */}
@@ -115,23 +150,25 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
         fill={COLORS.LIGHT}
         stroke={COLORS.DARK}
         strokeWidth={2}
-        vectorEffect="non-scaling-stroke"
+        // vectorEffect="non-scaling-stroke"
         pointerEvents="auto"
       />
       <rect
-        x={position.x + nodeWidth / 2 - COMPONENT_LABEL_WIDTH / 2}
-        y={-position.y + COMPONENT_INTERNAL_PADDING}
-        width={COMPONENT_LABEL_WIDTH}
-        height={nodeHeight - COMPONENT_INTERNAL_PADDING * 2}
+        x={position.x + nodeWidth / 2 - NODE_LABEL_WIDTH / 2}
+        y={-position.y + NODE_INTERNAL_PADDING}
+        width={NODE_LABEL_WIDTH}
+        height={nodeHeight - NODE_INTERNAL_PADDING * 2}
         rx={7}
         ry={7}
         fill={COLORS.LIGHT}
         stroke={COLORS.DARK}
         strokeWidth={2}
-        vectorEffect="non-scaling-stroke"
+        // vectorEffect="non-scaling-stroke"
         pointerEvents="none"
       />
       {inputPorts}
+      {inputPortLabels}
+      {nodeLabel}
     </g>
   )
 }
