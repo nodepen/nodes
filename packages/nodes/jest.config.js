514e9d053ebd7c183e8b66cf21075bce2d306ae6
Chuck Driesler <cdriesler.iv@gmail.com>
2022-11-09 22:39:47 -0500

    fix jest config

--- packages/nodes/jest.config.js
+++ packages/nodes/jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
 };
