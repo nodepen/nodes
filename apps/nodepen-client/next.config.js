8c88b35273798863878cf8bd88dded4aba1ed899
Chuck Driesler <cdriesler.iv@gmail.com>
2022-12-25 13:07:54 -0800

    bump next

--- apps/nodepen-client/next.config.js
+++ apps/nodepen-client/next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const withTM = require('next-transpile-modules')(['@nodepen/nodes', '@nodepen/core'], { resolveSymlinks: false })
 const withImages = require('next-images')
 
 const nextConfig = {
@@ -10,9 +9,7 @@ const nextConfig = {
   },
   images: {
     disableStaticImages: true,
-  },
+  }
 }
 
 module.exports = withImages(nextConfig)
-
-// module.exports = withImages(withTM(nextConfig))
