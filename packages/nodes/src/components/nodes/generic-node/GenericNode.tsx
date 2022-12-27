63703a7d8c3aaafcece0ab70a339d4cbca96028b
Chuck Driesler <cdriesler.iv@gmail.com>
2022-12-26 18:00:27 -0800

    component-based port labels

--- packages/nodes/src/components/nodes/generic-node/GenericNode.tsx
+++ packages/nodes/src/components/nodes/generic-node/GenericNode.tsx
@@ -22,203 +22,158 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
 
   const draggableTargetRef = useDraggableNode(id)
 
-  const { position, sources } = node
-  const { nickName, inputs, outputs } = template
+  const { position } = node
 
-  console.log(`⚙️⚙️⚙️ Rendered generic node [${id.split('-')[0]}] (${nickName})`)
+  console.log(`⚙️⚙️⚙️ Rendered generic node [${id.split('-')[0]}] (${template.nickName})`)
 
   const nodeWidth = getNodeWidth()
-
   const nodeHeight = getNodeHeight(template)
-  const nodeContentHeight = nodeHeight - NODE_INTERNAL_PADDING * 2
 
-  // TODO: Refactor out repetition, extract to util, add tests
-  const getPortColumnPosition = (orderNumber: number, direction: 'input' | 'output'): [x: number, y: number] => {
-    switch (direction) {
-      case 'input': {
-        const x = position.x
+  // const inputPortShadows = inputs.map((input, i) => {
+  //   const [x, y] = getPortColumnPosition(i, 'input')
 
-        const columnVerticalStep = inputs.length === 0 ? nodeContentHeight : nodeContentHeight / inputs.length
+  //   return (
+  //     <circle
+  //       key={`generic-node-input-port-shadow-${input.name}`}
+  //       r={NODE_PORT_RADIUS}
+  //       cx={x}
+  //       cy={y + 2}
+  //       fill={COLORS.DARK}
+  //       stroke={COLORS.DARK}
+  //       strokeWidth={2}
+  //       // vectorEffect="non-scaling-stroke"
+  //     />
+  //   )
+  // })
 
-        const currentVerticalNodePosition = -position.y
-        const currentVerticalPadding = NODE_INTERNAL_PADDING
-        const currentVerticalPortPosition = orderNumber * columnVerticalStep
-        const currentVerticalPortCenterOffset = columnVerticalStep / 2
+  // const inputPortLabels = inputs.map((input, i) => {
+  //   const { nickName } = input
 
-        const y =
-          currentVerticalNodePosition +
-          currentVerticalPadding +
-          currentVerticalPortPosition +
-          currentVerticalPortCenterOffset
+  //   const [x, y] = getPortColumnPosition(i, 'input')
 
-        return [x, y]
-      }
-      case 'output': {
-        const x = position.x + nodeWidth
+  //   return (
+  //     <text
+  //       key={`generic-node-input-port-label-${input.name}`}
+  //       x={x + 12}
+  //       y={y - 3 + NODE_PORT_LABEL_FONT_SIZE / 2}
+  //       className="np-font-mono np-select-none"
+  //       fontSize={NODE_PORT_LABEL_FONT_SIZE}
+  //       fill={COLORS.DARK}
+  //       onClick={(e) => {
+  //         e.stopPropagation()
 
-        const columnVerticalStep = outputs.length === 0 ? nodeContentHeight : nodeContentHeight / outputs.length
+  //         const portInstanceId = Object.entries(node.inputs).find(([instanceId, order]) => order === input.__order)?.[0]
 
-        const currentVerticalNodePosition = -position.y
-        const currentVerticalPadding = NODE_INTERNAL_PADDING
-        const currentVerticalPortPosition = orderNumber * columnVerticalStep
-        const currentVerticalPortCenterOffset = columnVerticalStep / 2
+  //         if (!portInstanceId) {
+  //           console.log('Not found')
+  //           return
+  //         }
 
-        const y =
-          currentVerticalNodePosition +
-          currentVerticalPadding +
-          currentVerticalPortPosition +
-          currentVerticalPortCenterOffset
+  //         apply((state) => {
+  //           state.document.configuration.pinnedPorts.push({
+  //             nodeInstanceId: node.instanceId,
+  //             portInstanceId: portInstanceId,
+  //           })
+  //         })
+  //       }}
+  //     >
+  //       {nickName}
+  //     </text>
+  //   )
+  // })
 
-        return [x, y]
-      }
-    }
-  }
+  // const outputPortShadows = outputs.map((output, i) => {
+  //   const [x, y] = getPortColumnPosition(i, 'output')
 
-  const inputPorts = inputs.map((input, i) => {
-    const [x, y] = getPortColumnPosition(i, 'input')
-
-    return (
-      <circle
-        key={`generic-node-input-port-${input.name}`}
-        r={NODE_PORT_RADIUS}
-        cx={x}
-        cy={y}
-        fill={COLORS.LIGHT}
-        stroke={COLORS.DARK}
-        strokeWidth={2}
-        // vectorEffect="non-scaling-stroke"
-      />
-    )
-  })
-
-  const inputPortShadows = inputs.map((input, i) => {
-    const [x, y] = getPortColumnPosition(i, 'input')
-
-    return (
-      <circle
-        key={`generic-node-input-port-shadow-${input.name}`}
-        r={NODE_PORT_RADIUS}
-        cx={x}
-        cy={y + 2}
-        fill={COLORS.DARK}
-        stroke={COLORS.DARK}
-        strokeWidth={2}
-        // vectorEffect="non-scaling-stroke"
-      />
-    )
-  })
-
-  const inputPortLabels = inputs.map((input, i) => {
-    const { nickName } = input
-
-    const [x, y] = getPortColumnPosition(i, 'input')
-
-    return (
-      <text
-        key={`generic-node-input-port-label-${input.name}`}
-        x={x + 12}
-        y={y - 3 + NODE_PORT_LABEL_FONT_SIZE / 2}
-        className="np-font-mono np-select-none"
-        fontSize={NODE_PORT_LABEL_FONT_SIZE}
-        fill={COLORS.DARK}
-        onClick={(e) => {
-          e.stopPropagation()
-
-          const portInstanceId = Object.entries(node.inputs).find(([instanceId, order]) => order === input.__order)?.[0]
-
-          if (!portInstanceId) {
-            console.log('Not found')
-            return
-          }
-
-          apply((state) => {
-            state.document.configuration.pinnedPorts.push({
-              nodeInstanceId: node.instanceId,
-              portInstanceId: portInstanceId,
-            })
-          })
-        }}
-      >
-        {nickName}
-      </text>
-    )
-  })
-
-  const outputPorts = outputs.map((output, i) => {
-    const [x, y] = getPortColumnPosition(i, 'output')
-
-    return (
-      <circle
-        key={`generic-node-output-port-${output.name}`}
-        r={NODE_PORT_RADIUS}
-        cx={x}
-        cy={y}
-        fill={COLORS.LIGHT}
-        stroke={COLORS.DARK}
-        strokeWidth={2}
-        // vectorEffect="non-scaling-stroke"
-      />
-    )
-  })
-
-  const outputPortShadows = outputs.map((output, i) => {
-    const [x, y] = getPortColumnPosition(i, 'output')
-
-    return (
-      <circle
-        key={`generic-node-output-port-shadow-${output.name}`}
-        r={NODE_PORT_RADIUS}
-        cx={x}
-        cy={y + 2}
-        fill={COLORS.DARK}
-        stroke={COLORS.DARK}
-        strokeWidth={2}
-        // vectorEffect="non-scaling-stroke"
-      />
-    )
-  })
-
-  const outputPortLabels = outputs.map((output, i) => {
-    const { nickName } = output
-
-    const [x, y] = getPortColumnPosition(i, 'output')
-
-    return (
-      <text
-        key={`generic-node-output-port-label-${output.name}`}
-        x={x - 12}
-        y={y - 3 + NODE_PORT_LABEL_FONT_SIZE / 2}
-        className="np-font-mono np-select-none"
-        fontSize={NODE_PORT_LABEL_FONT_SIZE}
-        fill={COLORS.DARK}
-        textAnchor="end"
-      >
-        {nickName}
-      </text>
-    )
-  })
-
-  const nodeLabel = (
-    <>
-      <path
-        id={`node-label-${id}`}
-        fill="none"
-        // stroke="red"
-        d={`M ${position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3} ${-position.y + nodeHeight} L ${
-          position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3
-        } ${-position.y}`}
-      />
-      <text className="np-font-panel np-select-none" fill={COLORS.DARK} fontSize={NODE_LABEL_FONT_SIZE}>
-        <textPath href={`#node-label-${id}`} startOffset="50%" textAnchor="middle" alignmentBaseline="middle">
-          {nickName.toUpperCase()}
-        </textPath>
-      </text>
-    </>
-  )
+  //   return (
+  //     <circle
+  //       key={`generic-node-output-port-shadow-${output.name}`}
+  //       r={NODE_PORT_RADIUS}
+  //       cx={x}
+  //       cy={y + 2}
+  //       fill={COLORS.DARK}
+  //       stroke={COLORS.DARK}
+  //       strokeWidth={2}
+  //       // vectorEffect="non-scaling-stroke"
+  //     />
+  //   )
+  // })
 
   return (
     <>
-      {Object.entries(sources).map(([inputPortInstanceId, sources]) => {
+      <g id={`generic-node-${id}`} ref={draggableTargetRef}>
+        {/* Shadow */}
+        <rect
+          x={position.x}
+          y={-position.y + 2}
+          width={nodeWidth}
+          height={nodeHeight}
+          rx={7}
+          ry={7}
+          fill={COLORS.DARK}
+          stroke={COLORS.DARK}
+          strokeWidth={2}
+        />
+        {/* Body */}
+        <rect
+          x={position.x}
+          y={-position.y}
+          width={nodeWidth}
+          height={nodeHeight}
+          rx={7}
+          ry={7}
+          fill={COLORS.LIGHT}
+          stroke={COLORS.DARK}
+          strokeWidth={2}
+          pointerEvents="auto"
+        />
+        {/* Label */}
+        <rect
+          x={position.x + nodeWidth / 2 - NODE_LABEL_WIDTH / 2}
+          y={-position.y + NODE_INTERNAL_PADDING}
+          width={NODE_LABEL_WIDTH}
+          height={nodeHeight - NODE_INTERNAL_PADDING * 2}
+          rx={7}
+          ry={7}
+          fill={COLORS.LIGHT}
+          stroke={COLORS.DARK}
+          strokeWidth={2}
+          pointerEvents="none"
+        />
+        <path
+          id={`node-label-${id}`}
+          fill="none"
+          d={`M ${position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3} ${-position.y + nodeHeight} L ${
+            position.x + nodeWidth / 2 + NODE_LABEL_FONT_SIZE / 2 - 3
+          } ${-position.y}`}
+        />
+        <text className="np-font-panel np-select-none" fill={COLORS.DARK} fontSize={NODE_LABEL_FONT_SIZE}>
+          <textPath href={`#node-label-${id}`} startOffset="50%" textAnchor="middle" alignmentBaseline="middle">
+            {template.nickName.toUpperCase()}
+          </textPath>
+        </text>
+        {/* Ports */}
+        {Object.entries(node.inputs).map(([inputPortInstanceId, order]) => (
+          <GenericNodePort
+            key={`generic-node-input-port-${inputPortInstanceId}`}
+            nodeInstanceId={id}
+            portInstanceId={inputPortInstanceId}
+            template={template.inputs[order]}
+            direction="input"
+          />
+        ))}
+        {Object.entries(node.outputs).map(([outputPortInstanceId, order]) => (
+          <GenericNodePort
+            key={`generic-node-output-port-${outputPortInstanceId}`}
+            nodeInstanceId={id}
+            portInstanceId={outputPortInstanceId}
+            template={template.outputs[order]}
+            direction="output"
+          />
+        ))}
+      </g>
+      {/* Wires */}
+      {Object.entries(node.sources).map(([inputPortInstanceId, sources]) => {
         const currentInput = {
           nodeInstanceId: node.instanceId,
           portInstanceId: inputPortInstanceId,
@@ -236,71 +191,6 @@ const GenericNode = ({ id, template }: GenericNodeProps): React.ReactElement => 
           return <Wire key={wireKey} from={source} to={currentInput} />
         })
       })}
-      <g id={`generic-node-${id}`} ref={draggableTargetRef}>
-        {/* Shadow */}
-        <rect
-          x={position.x}
-          y={-position.y + 2}
-          width={nodeWidth}
-          height={nodeHeight}
-          rx={7}
-          ry={7}
-          fill={COLORS.DARK}
-          stroke={COLORS.DARK}
-          strokeWidth={2}
-          // vectorEffect="non-scaling-stroke"
-        />
-        {/* {inputPortShadows} */}
-        {/* {outputPortShadows} */}
-        {/* Body */}
-        <rect
-          x={position.x}
-          y={-position.y}
-          width={nodeWidth}
-          height={nodeHeight}
-          rx={7}
-          ry={7}
-          fill={COLORS.LIGHT}
-          stroke={COLORS.DARK}
-          strokeWidth={2}
-          // vectorEffect="non-scaling-stroke"
-          pointerEvents="auto"
-        />
-        <rect
-          x={position.x + nodeWidth / 2 - NODE_LABEL_WIDTH / 2}
-          y={-position.y + NODE_INTERNAL_PADDING}
-          width={NODE_LABEL_WIDTH}
-          height={nodeHeight - NODE_INTERNAL_PADDING * 2}
-          rx={7}
-          ry={7}
-          fill={COLORS.LIGHT}
-          stroke={COLORS.DARK}
-          strokeWidth={2}
-          // vectorEffect="non-scaling-stroke"
-          pointerEvents="none"
-        />
-        {/* {inputPorts} */}
-        {/* {outputPorts} */}
-        {/* {inputPortLabels} */}
-        {/* {outputPortLabels} */}
-        {Object.keys(node.inputs).map((inputPortInstanceId) => (
-          <GenericNodePort
-            key={`generic-node-input-port-${inputPortInstanceId}`}
-            nodeInstanceId={id}
-            portInstanceId={inputPortInstanceId}
-            direction="input"
-          />
-        ))}
-        {Object.keys(node.outputs).map((outputPortInstanceId) => (
-          <GenericNodePort
-            key={`generic-node-output-port-${outputPortInstanceId}`}
-            nodeInstanceId={id}
-            portInstanceId={outputPortInstanceId}
-            direction="output"
-          />
-        ))}
-        {nodeLabel}
-      </g>
     </>
   )
 }
--- packages/nodes/src/components/nodes/generic-node/components/GenericNodePort.tsx
+++ packages/nodes/src/components/nodes/generic-node/components/GenericNodePort.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import type * as NodePen from '@nodepen/core'
 import { useNodeAnchorPosition } from '@/hooks'
 import { COLORS, DIMENSIONS } from '@/constants'
 
-const { NODE_PORT_RADIUS } = DIMENSIONS
+const { NODE_PORT_LABEL_FONT_SIZE, NODE_PORT_RADIUS } = DIMENSIONS
 
 type GenericNodePortProps = {
     nodeInstanceId: string
     portInstanceId: string
+    template: NodePen.PortTemplate
     direction: 'input' | 'output'
 }
 
-const GenericNodePort = ({ nodeInstanceId, portInstanceId, direction }: GenericNodePortProps) => {
+const GenericNodePort = ({ nodeInstanceId, portInstanceId, template, direction }: GenericNodePortProps) => {
     const position = useNodeAnchorPosition(nodeInstanceId, portInstanceId)
 
     if (!position) {
@@ -20,15 +22,38 @@ const GenericNodePort = ({ nodeInstanceId, portInstanceId, direction }: GenericN
 
     const { x, y } = position
 
+    const labelPosition = {
+        x: direction === 'input'
+            ? x + 12
+            : x - 12,
+        y: y - 1.5 - NODE_PORT_LABEL_FONT_SIZE / 4
+    }
+
+    const labelAnchor = direction === 'input'
+        ? 'start'
+        : 'end'
+
     return (
-        <circle
-        r={NODE_PORT_RADIUS}
-        cx={x}
-        cy={-y}
-        fill={COLORS.LIGHT}
-        stroke={COLORS.DARK}
-        strokeWidth={2}
-      />
+        <>
+            <circle
+                r={NODE_PORT_RADIUS}
+                cx={x}
+                cy={-y}
+                fill={COLORS.LIGHT}
+                stroke={COLORS.DARK}
+                strokeWidth={2}
+            />
+            <text
+                x={labelPosition.x}
+                y={-labelPosition.y}
+                className="np-font-mono np-select-none"
+                fontSize={NODE_PORT_LABEL_FONT_SIZE}
+                fill={COLORS.DARK}
+                textAnchor={labelAnchor}
+            >
+                {template.nickName}
+            </text>
+        </>
     )
 }
 
