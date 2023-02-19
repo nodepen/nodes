3451db34dc5bb1f63c30724bbc88697dc50a388e
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-19 15:05:15 +0000

    respect prefers-reduced-motion

--- packages/nodes/src/views/document-view/components/context-menu-container/ContextMenuContainer.tsx
+++ packages/nodes/src/views/document-view/components/context-menu-container/ContextMenuContainer.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { useStore } from '$'
 import { PortContextMenu } from './menus'
 import { getMenuHeight } from './utils'
-import { trySetVariable } from '@/utils/css'
+import { useReducedMotion } from '@/hooks'
 
 const ContextMenuContainer = () => {
     const menus = useStore((state) => Object.entries(state.registry.contextMenus))
+    const prefersReducedMotion = useReducedMotion()
 
     return (
         <div className="np-w-full np-h-full np-pointer-events-none np-relative">
             {menus.map(([key, menu]) => {
                 const contextType = menu.context.type
 
-                const menuHeight = getMenuHeight(menu.context)
-                trySetVariable('--np-active-menu-height', `${menuHeight}px`)
+                if (!prefersReducedMotion) {
+                    const menuHeight = getMenuHeight(menu.context)
+                    document.documentElement.style.setProperty('--np-active-menu-height', `${menuHeight}px`)
+                }
 
                 switch (contextType) {
                     case 'port': {
