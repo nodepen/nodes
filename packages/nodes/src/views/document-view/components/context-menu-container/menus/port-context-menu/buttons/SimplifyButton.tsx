365640caa07bfc08ef8fe8d5e9ae02a8ad0e35a8
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-19 15:39:51 +0000

    better simplify button

--- packages/nodes/src/views/document-view/components/context-menu-container/menus/port-context-menu/buttons/SimplifyButton.tsx
+++ packages/nodes/src/views/document-view/components/context-menu-container/menus/port-context-menu/buttons/SimplifyButton.tsx
@@ -18,7 +18,7 @@ export const SimplifyButton = ({ nodeInstanceId, portInstanceId }: SimplifyButto
     const icon = (
         <div className='np-w-[18px] np-h-[18px] np-rounded-sm np-bg-light np-border-2 np-border-dark np-flex np-justify-center np-items-center'>
             <svg {...STYLES.BUTTON.SMALL} width={14} height={14}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M 18 12 H 12 L 6 6 M 12 12 L 6 18" />
             </svg>
         </div>
     )
