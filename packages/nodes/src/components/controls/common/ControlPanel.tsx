0194ff39dd7afe0283459a386db5fb4421e73926
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-25 18:24:32 +0000

    active tab translates with selection

--- packages/nodes/src/components/controls/common/ControlPanel.tsx
+++ packages/nodes/src/components/controls/common/ControlPanel.tsx
@@ -3,15 +3,16 @@ import { usePseudoShadow } from '@/views/common/pseudo-shadow'
 
 type ControlPanelProps = {
   children: React.ReactNode
+  disablePadding?: boolean
 }
 
-export const ControlPanel = ({ children }: ControlPanelProps): React.ReactElement => {
+export const ControlPanel = ({ children, disablePadding }: ControlPanelProps): React.ReactElement => {
   const shadowTargetRef = usePseudoShadow()
 
   return (
     <div
       ref={shadowTargetRef}
-      className="np-w-full np-p-4 np-mb-3 np-bg-green np-rounded-md np-shadow-main np-pointer-events-auto"
+      className={`${disablePadding ? '' : 'np-p-4'} np-w-full np-mb-3 np-bg-green np-rounded-md np-shadow-main np-pointer-events-auto`}
     >
       {children}
     </div>
--- packages/nodes/src/components/controls/panels/active-view-control/ActiveViewControl.tsx
+++ packages/nodes/src/components/controls/panels/active-view-control/ActiveViewControl.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { ControlPanel } from '../../common'
 import { useStore, useDispatch } from '$'
 import { clamp } from '@/utils'
-import { useViewPosition } from '@/views/common/hooks'
+import { useActiveViewPosition, useViewPosition } from '@/views/common/hooks'
 import { COLORS } from '@/constants'
 
 export const ActiveViewControl = (): React.ReactElement => {
@@ -11,97 +11,60 @@ export const ActiveViewControl = (): React.ReactElement => {
   const registeredViews = useStore((store) => Object.keys(store.registry.views))
 
   return (
-    <ControlPanel>
-      <div className="np-w-full np-h-4 np-relative np-overflow-hidden">
-        {registeredViews.map((viewKey) => (
-          <ActiveViewLabel key={`view-control-tab-${viewKey}`} viewKey={viewKey} />
-        ))}
-        <div
-          className="np-w-3 np-h-4 np-absolute np-flex np-justify-center np-items-center hover:np-cursor-pointer np-bg-green"
-          style={{ left: 0, top: 0, zIndex: 20 }}
-          onClick={() =>
-            apply((state) => {
-              const viewCount = Object.keys(state.registry.views).length
-              const activeView = state.layout.activeView
-
-              if (!activeView) {
-                return
-              }
-
-              const activeViewConfiguration = state.registry.views[activeView]
-
-              if (!activeViewConfiguration) {
-                return
-              }
-
-              const nextViewPosition = clamp(activeViewConfiguration.order - 1, 0, viewCount - 1)
-              const nextViewKey = Object.entries(state.registry.views).find(
-                ([_key, config]) => config.order === nextViewPosition
-              )?.[0]
-
-              if (!nextViewKey) {
-                return
-              }
-
-              state.layout.activeView = nextViewKey
-            })
-          }
-        >
-          <svg width={4} height={8} viewBox="0 0 5 10" className="np-overflow-visible">
-            <polyline
-              points="5,0 0,5 5,10"
-              fill="none"
-              stroke={COLORS.DARKGREEN}
-              strokeWidth="2px"
-              vectorEffect="non-scaling-stroke"
-              strokeLinecap="round"
-            />
-          </svg>
+    <div className='np-w-full np-grid np-grid-cols-[min-content_1fr] np-gap-3'>
+      <ControlPanel disablePadding>
+        <div className='np-h-10 np-w-14 np-p-1 np-flex np-items-center np-justify-center'>
+          <button className='np-w-8 np-h-8 np-flex np-items-center np-justify-center np-rounded-sm hover:np-bg-swampgreen'>
+            <svg width={20} aria-hidden="true" fill="none" stroke={COLORS.DARKGREEN} strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" strokeLinecap="round" strokeLinejoin="round" vectorEffect={"non-scaling-stroke"} />
+            </svg>
+          </button>
         </div>
+      </ControlPanel>
+      <ControlPanel disablePadding>
+        <ActiveViewTabs />
+      </ControlPanel>
+    </div>
+  )
+}
+
+const ActiveViewTabs = () => {
+  const { apply } = useDispatch()
+
+  const registeredViewKeys = useStore((state) => Object.keys(state.registry.views))
+  const activeViewPosition = useActiveViewPosition()
+
+  return (
+    <div className='np-w-full np-h-full np-relative'>
+      <div className='np-absolute np-w-full np-h-full np-p-1 np-left-0 np-top-0 np-z-0'>
         <div
-          className="np-w-3 np-h-4 np-absolute np-flex np-justify-center np-items-center hover:np-cursor-pointer np-bg-green"
-          style={{ right: 0, top: 0, zIndex: 20 }}
-          onClick={() =>
-            apply((state) => {
-              const viewCount = Object.keys(state.registry.views).length
-              const activeView = state.layout.activeView
-
-              if (!activeView) {
-                return
-              }
-
-              const activeViewConfiguration = state.registry.views[activeView]
-
-              if (!activeViewConfiguration) {
-                return
-              }
-
-              const nextViewPosition = clamp(activeViewConfiguration.order + 1, 0, viewCount - 1)
-              const nextViewKey = Object.entries(state.registry.views).find(
-                ([_key, config]) => config.order === nextViewPosition
-              )?.[0]
-
-              if (!nextViewKey) {
-                return
-              }
-
-              state.layout.activeView = nextViewKey
-            })
-          }
-        >
-          <svg width={4} height={8} viewBox="0 0 5 10" className="np-overflow-visible">
-            <polyline
-              points="0,0 5,5 0,10"
-              fill="none"
-              stroke={COLORS.DARKGREEN}
-              strokeWidth="2px"
-              vectorEffect="non-scaling-stroke"
-              strokeLinecap="round"
-            />
-          </svg>
+          className='np-w-8 np-h-8 np-rounded-sm np-bg-swampgreen'
+          style={{
+            transition: 'transform',
+            transitionDuration: '300ms',
+            transitionTimingFunction: 'ease-out',
+            transform: `translateX(${clamp(activeViewPosition, 0, 10) * (32 + 4)}px)`
+          }}
+        />
+      </div>
+      <div className='np-absolute np-w-full np-h-full np-p-1 np-left-0 np-top-0 np-z-10'>
+        <div className='np-w-full np-h-full np-flex np-justify-start np-items-center'>
+          {registeredViewKeys.map((viewKey) => (
+            <button
+              key={`navigation-tab-button-${viewKey}`}
+              className='np-w-8 np-h-8 np-mr-1 last:np-mr-0 np-pointer-events-auto np-flex np-items-center np-justify-center'
+              onClick={() => {
+                apply((state) => {
+                  state.layout.activeView = viewKey
+                })
+              }}
+            >
+              O
+            </button>
+          ))}
         </div>
       </div>
-    </ControlPanel>
+    </div>
   )
 }
 
