a5f2a5bc9159e7bfc305b03bb6ec982228b4d9d3
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-26 16:07:24 +0000

    like this better, actually

--- packages/nodes/src/components/controls/panels/template-library-control/TemplateLibraryControl.tsx
+++ packages/nodes/src/components/controls/panels/template-library-control/TemplateLibraryControl.tsx
@@ -18,8 +18,6 @@ const TemplateLibraryControl = ({ templates }: TemplateLibraryControlProps): Rea
     setActiveCategory(templateCategories[0])
   }
 
-  const [overflows, setOverflows] = useState<('left' | 'right')[]>(['right'])
-
   const icon = (
     <svg width={24} aria-hidden="true" fill="none" stroke={COLORS.DARKGREEN} strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" strokeLinecap="round" strokeLinejoin="round" />
@@ -28,34 +26,17 @@ const TemplateLibraryControl = ({ templates }: TemplateLibraryControlProps): Rea
 
   return (
     <ControlPanel>
-      <ControlPanelHeader
+      {/* <ControlPanelHeader
         icon={icon}
         label="Component Library"
         onClickMenu={() => ''}
-      />
+      /> */}
       <div
-        className={`np-border-l-2 np-border-r-2 ${overflows.includes('left') ? 'np-border-l-swampgreen' : 'np-border-l-green'} ${overflows.includes('right') ? 'np-border-r-swampgreen' : 'np-border-r-green'} np-w-full np-pb-[1px] np-rounded-sm np-overflow-auto np-whitespace-nowrap no-scrollbar`}
-        onScroll={(e) => {
-          const { scrollLeft, scrollWidth, clientWidth } = e.currentTarget
-
-          const currentOverflows: ('left' | 'right')[] = []
-
-          if (scrollLeft > 0) {
-            currentOverflows.push('left')
-          }
-
-          const maximumScroll = scrollWidth - clientWidth
-
-          if (scrollLeft < maximumScroll) {
-            currentOverflows.push('right')
-          }
-
-          setOverflows(currentOverflows)
-        }}
+        className={`np-shadow-input np-bg-pale np-w-full np-p-2 np-pt-1 np-pb-0 np-rounded-sm np-overflow-auto np-whitespace-nowrap no-scrollbar`}
       >
         {templateCategories.map((category) => (
           <button
-            className={`${category === activeCategory ? 'np-bg-swampgreen np-border-b-swampgreen' : 'np-border-b-green'} np-inline-block np-box-border np-h-6 np-mr-1 last:np-mr-0 np-rounded-sm np-border-b-2 hover:np-border-b-swampgreen`}
+            className={`${category === activeCategory ? 'np-bg-green np-border-b-green np-border-b-2 np-shadow-main' : ''} np-inline-block np-box-border np-h-6 np-mr-1 last:np-mr-0 np-rounded-sm np-rounded-br-none np-rounded-bl-none hover:np-border-b-2 hover:np-border-b-green`}
             onClick={() => { setActiveCategory(category) }}
           >
             <p className='np-pl-2 np-pr-2 np-font-sans np-text-sm np-text-dark np-select-none -np-translate-y-px hover:np-translate-y-[-2px]'>
