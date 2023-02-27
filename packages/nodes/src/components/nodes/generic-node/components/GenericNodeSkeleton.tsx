ea4d012190c69007e1b40c5138bee8b71caf1c7c
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-27 22:57:01 +0000

    better dash offset math

--- packages/nodes/src/components/nodes/generic-node/components/GenericNodeSkeleton.tsx
+++ packages/nodes/src/components/nodes/generic-node/components/GenericNodeSkeleton.tsx
@@ -14,22 +14,41 @@ export const GenericNodeSkeleton = ({ node, template }: GenericNodeSkeletonProps
     const nodeWidth = useMemo(() => getNodeWidth(), [])
     const nodeHeight = useMemo(() => getNodeHeight(template), [template])
 
+    const NODE_CORNER_RADIUS = 7
+
+    const pathLength = [
+        2 * Math.PI * NODE_CORNER_RADIUS,
+        2 * (nodeHeight - (2 * NODE_CORNER_RADIUS)),
+        2 * (nodeWidth - (2 * NODE_CORNER_RADIUS))
+    ].reduce((partial, value) => value + partial, 0)
+
+    const NODE_TARGET_DASH = 15
+
+    const dashSegmentCount = Math.floor(pathLength / NODE_TARGET_DASH)
+    const dashSegmentOverflow = pathLength % dashSegmentCount
+
+    const dashSegmentLength = NODE_TARGET_DASH + (dashSegmentOverflow / dashSegmentCount)
+
+    const dashSegmentFraction = dashSegmentLength / NODE_TARGET_DASH
+    const dashSegmentDash = dashSegmentFraction * 9
+    const dashSegmentGap = dashSegmentFraction * 6
+
     return (
         <rect
-            className='np-animate-march '
+            className='np-animate-march'
             x={position.x}
             y={position.y}
             width={nodeWidth}
             height={nodeHeight}
-            rx={7}
-            ry={7}
+            rx={NODE_CORNER_RADIUS}
+            ry={NODE_CORNER_RADIUS}
             fill="none"
             stroke={COLORS.DARK}
             strokeWidth={2}
             pointerEvents="none"
             vectorEffect="non-scaling-stroke"
-            strokeDasharray={"9 6"}
-            strokeDashoffset={"15"}
+            strokeDasharray={`${dashSegmentDash} ${dashSegmentGap}`}
+            strokeDashoffset={`${dashSegmentLength}`}
         />
     )
 }
