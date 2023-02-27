193ef4aa3fb795b6a9e4035e312541f574da03fc
Chuck Driesler <cdriesler.iv@gmail.com>
2023-02-27 23:16:06 +0000

    cute lil sticky moment

--- packages/nodes/src/components/controls/panels/template-library-control/TemplateLibraryControl.tsx
+++ packages/nodes/src/components/controls/panels/template-library-control/TemplateLibraryControl.tsx
@@ -34,7 +34,7 @@ const TemplateLibraryControl = ({ templates }: TemplateLibraryControlProps): Rea
       >
         {templateCategories.map((category) => (
           <button
-            className={`${category === activeCategory ? 'np-bg-green np-border-b-green np-border-b-2 np-shadow-main' : ''} np-inline-block np-box-border np-h-6 np-mr-1 last:np-mr-2 np-rounded-sm np-rounded-br-none np-rounded-bl-none hover:np-border-b-2 hover:np-border-b-green`}
+            className={`${category === activeCategory ? 'np-bg-green np-border-b-green np-border-b-2 np-shadow-main np-sticky np-left-0 np-right-0 np-z-20' : 'np-z-10'} np-inline-block np-box-border np-h-6 np-mr-1 last:np-mr-2 np-rounded-sm np-rounded-br-none np-rounded-bl-none hover:np-border-b-2 hover:np-border-b-green`}
             onClick={() => { setActiveCategory(category) }}
           >
             <p className='np-pl-2 np-pr-2 np-font-sans np-text-sm np-text-dark np-select-none -np-translate-y-px hover:np-translate-y-[-2px]'>
@@ -45,64 +45,46 @@ const TemplateLibraryControl = ({ templates }: TemplateLibraryControlProps): Rea
       </div>
       <div className='np-w-full np-max-h-[148px] np-overflow-auto no-scrollbar'>
         {Object.entries(activeCategoryTemplatesBySubcategory).map(([subcategory, templates]) => (
-          <>
-            {/* <div className='np-sticky np-top-0 np-pl-2 np-p-1 np-mb-2 np-bg-green np-z-10'>
-              <p className='np-font-sans np-text-sm np-text-darkgreen np-select-none -np-translate-y-px'>{subcategory}</p>
-            </div> */}
-            <div className='np-w-full np-pb-2 last:np-pb-0 np-mb-2 last:np-mb-0 np-border-b-2 np-border-b-swampgreen last:np-border-none np-grid np-grid-cols-[repeat(auto-fill,_minmax(30px,_1fr))] np-gap-2 np-z-0'>
-              {templates.map((template) => (
-                <button
-                  className='np-w-full np-h-full np-pt-[100%] np-relative hover:np-bg-swampgreen np-rounded-sm'
-                  onPointerDown={(e) => {
-                    const { pageX, pageY } = e
+          <div className='np-w-full np-pb-2 last:np-pb-0 np-mb-2 last:np-mb-0 np-border-b-2 np-border-b-swampgreen last:np-border-none np-grid np-grid-cols-[repeat(auto-fill,_minmax(30px,_1fr))] np-gap-2 np-z-0'>
+            {templates.map((template) => (
+              <button
+                className='np-w-full np-h-full np-pt-[100%] np-relative hover:np-bg-swampgreen np-rounded-sm'
+                onPointerDown={(e) => {
+                  const { pageX, pageY } = e
 
-                    const [x, y] = pageSpaceToWorldSpace(pageX, pageY)
+                  const [x, y] = pageSpaceToWorldSpace(pageX, pageY)
 
-                    const nodeWidth = getNodeWidth()
-                    const nodeHeight = getNodeHeight(template)
+                  const nodeWidth = getNodeWidth()
+                  const nodeHeight = getNodeHeight(template)
 
-                    apply((state) => {
-                      const node = createInstance(template)
+                  apply((state) => {
+                    const node = createInstance(template)
 
-                      node.status.isProvisional = true
-                      node.position = {
-                        x: x - nodeWidth / 2,
-                        y: y - nodeHeight / 2
-                      }
+                    node.status.isProvisional = true
+                    node.position = {
+                      x: x - nodeWidth / 2,
+                      y: y - nodeHeight / 2
+                    }
 
-                      state.document.nodes[node.instanceId] = node
+                    state.document.nodes[node.instanceId] = node
 
-                      state.layout.nodePlacement = {
-                        isActive: true,
-                        activeNodeId: node.instanceId,
-                      }
-                    })
-                  }}
-                >
-                  <div className='np-absolute np-top-0 np-right-0 np-left-0 np-bottom-0'>
-                    <div className='np-w-full np-h-full np-flex np-items-center np-justify-center'>
-                      <img width={22} draggable={false} src={getIconAsImage(template)} />
-                    </div>
+                    state.layout.nodePlacement = {
+                      isActive: true,
+                      activeNodeId: node.instanceId,
+                    }
+                  })
+                }}
+              >
+                <div className='np-absolute np-top-0 np-right-0 np-left-0 np-bottom-0'>
+                  <div className='np-w-full np-h-full np-flex np-items-center np-justify-center'>
+                    <img width={22} draggable={false} src={getIconAsImage(template)} />
                   </div>
-                </button>
-              ))}
-            </div>
-          </>
+                </div>
+              </button>
+            ))}
+          </div>
         ))}
       </div>
-      {/* {Object.entries(templatesByCategory).map(([category, subcategoryNodes]) => (
-        <div>
-          <h3>{category}</h3>
-          {Object.entries(subcategoryNodes).map(([subcategory, templates]) => (
-            <>
-              <h4>{subcategory}</h4>
-              {templates.map((template) => (
-                <p>{template.nickName}</p>
-              ))}
-            </>
-          ))}
-        </div>
-      ))} */}
     </ControlPanel>
   )
 }
