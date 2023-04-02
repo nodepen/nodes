ea72a659b28edca68fe3c26a548fe2db9e32aaa6
Chuck Driesler <cdriesler.iv@gmail.com>
2023-04-02 19:11:24 +0100

    branch eslint config for nextjs project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@next/next/recommended',
     'plugin:@typescript-eslint/recommended',
     "plugin:jsx-a11y/recommended",
     'prettier'
