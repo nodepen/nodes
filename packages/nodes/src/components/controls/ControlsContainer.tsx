c54a2a3e8a706463f7fe40db76fd8173f13b60f8
Chuck Driesler <cdriesler.iv@gmail.com>
2023-04-14 21:37:58 +0100

    clear interface on controls click

--- packages/nodes/src/components/controls/ControlsContainer.tsx
+++ packages/nodes/src/components/controls/ControlsContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layer } from '@/views/common'
-import { useStore } from '$'
+import { useDispatch, useStore } from '$'
 import { ActiveViewControl, DocumentInfoControl, PinnedPortsControl, TemplateLibraryControl } from './panels'
 
 const ControlsContainer = (): React.ReactElement => {
@@ -26,11 +26,13 @@ type LayoutProps = {
 const ControlsContainerLayout = ({ children }: LayoutProps): React.ReactElement => {
   const shadowResizeProxyRef = useStore((state) => state.registry.shadows.proxyRefs['controls'])
 
+  const { clearInterface } = useDispatch()
+
   return (
     <Layer fixed id="np-controls-layer" z={90}>
       <div className="np-w-full np-h-full np-relative">
         <div className="np-w-full np-h-full np-overflow-hidden np-absolute np-flex np-flex-row np-justify-start np-items-center np-pointer-events-none np-z-50">
-          <div className="np-h-full np-w-72 np-p-4 np-flex np-flex-col">
+          <div className="np-h-full np-w-72 np-p-4 np-flex np-flex-col" onPointerDownCapture={clearInterface}>
             <div id="np-control-panels" className="np-w-full np-flex-grow np-flex np-flex-col">
               <div ref={shadowResizeProxyRef} className="np-w-full np-flex np-flex-col">
                 {children}
