3d7fdef7d229105b57ce070c1157b88e136d39b7
Chuck Driesler <cdriesler.iv@gmail.com>
2023-06-26 21:49:51 +0100

    lint

--- packages/nodes/src/views/document-view/layers/transient-element-overlay/tooltips/port-tooltip/components/DataTreePreviewEntry.tsx
+++ packages/nodes/src/views/document-view/layers/transient-element-overlay/tooltips/port-tooltip/components/DataTreePreviewEntry.tsx
@@ -14,14 +14,16 @@ export const DataTreePreviewEntry = ({ index, value, showBackground = false }: D
   return (
     <>
       <div
-        className={`${showBackground ? 'np-bg-grey-2' : ''
-          } np-p-1 np-pr-2 np-pb-0 np-rounded-tl-sm np-rounded-bl-sm np-text-right np-font-semibold`}
+        className={`${
+          showBackground ? 'np-bg-grey-2' : ''
+        } np-p-1 np-pr-2 np-pb-0 np-rounded-tl-sm np-rounded-bl-sm np-text-right np-font-semibold`}
       >
         <div className="-np-translate-y-px">{index}</div>
       </div>
       <div
-        className={`${showBackground ? 'np-bg-grey-2' : ''
-          } np-p-1 np-pb-0 np-rounded-tr-sm np-rounded-br-sm np-font-medium`}
+        className={`${
+          showBackground ? 'np-bg-grey-2' : ''
+        } np-p-1 np-pb-0 np-rounded-tr-sm np-rounded-br-sm np-font-medium`}
       >
         <div className="-np-translate-y-px">{valueString}</div>
       </div>
