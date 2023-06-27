1602a06698ac7daf44514091d1e3658b6e4bae04
Chuck Driesler <cdriesler.iv@gmail.com>
2023-06-27 18:56:10 +0100

    add list size to data tree summary

--- packages/nodes/src/utils/data-trees/getDataTreeSummary.ts
+++ packages/nodes/src/utils/data-trees/getDataTreeSummary.ts
@@ -1,6 +1,7 @@
 import type { DataTree } from '@nodepen/core'
 import { getDataTreeStructure } from '.'
 import { getDataTreeTypes } from './getDataTreeTypes'
+import { getDataTreeValueCountByPath } from './getDataTreeValueCountByPath'
 
 export const getDataTreeSummary = (data?: DataTree | null): string => {
   if (!data) {
@@ -19,13 +20,22 @@ export const getDataTreeSummary = (data?: DataTree | null): string => {
       const values = Object.values(data)[0]
       const types = getDataTreeTypes(data)
 
-      return `1 list of ${types.length === 1 ? types[0] : 'value'}${values.length === 1 ? '' : 's'}`
+      return `1 list of ${values.length} ${types.length === 1 ? types[0] : 'value'}${values.length === 1 ? '' : 's'}`
     }
     case 'tree': {
       const depth = Object.keys(data).length
       const types = getDataTreeTypes(data)
 
-      return `${depth} list${depth === 1 ? '' : 's'} of ${types.length === 1 ? types[0] : 'value'}s`
+      const [minCount, maxCount] = getDataTreeValueCountByPath(data)
+      console.log([minCount, maxCount])
+      const isUniformLength = minCount === maxCount
+
+      return [
+        `${depth} list${depth === 1 ? '' : 's'}`,
+        'of',
+        `${isUniformLength ? minCount : `${minCount} to ${maxCount}`}`,
+        `${types.length === 1 ? types[0] : 'value'}${isUniformLength && minCount === 1 ? '' : 's'}`,
+      ].join(' ')
     }
   }
 }
