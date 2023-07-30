8a2f7ce662f8bd8da1e765d18a7267b1aa6e6118
Chuck Driesler <cdriesler.iv@gmail.com>
2023-07-30 20:13:28 +0100

    get overall width and height (I think?)

--- packages/nodes/src/utils/node-dimensions/getNodeDimensions.ts
+++ packages/nodes/src/utils/node-dimensions/getNodeDimensions.ts
@@ -1,5 +1,7 @@
 import type * as NodePen from '@nodepen/core'
 import { DIMENSIONS } from '@/constants'
+import { clamp } from '@/utils/numerics'
+import { getLabelWidth } from './getLabelWidth'
 
 // Steps:
 // Collect node information (templates and port configurations)
@@ -8,6 +10,8 @@ import { DIMENSIONS } from '@/constants'
 // When rendering, *use those values* *reactively*, do not compute anything in components
 // ...
 // On a change that modifies dimensions, run this and update values in state.
+
+// labels: 12px per character, 22px per flag badge
 
 export const getNodeDimensions = (
   node: NodePen.DocumentNode,
@@ -21,22 +25,51 @@ export const getNodeDimensions = (
     },
   }
 
+  const inputLabelWidths: Record<string, number> = {}
+
+  for (const [instanceId, orderIndex] of Object.entries(node.inputs)) {
+    const portTemplate = nodeTemplate.inputs[orderIndex]
+    const portConfiguration = node.portConfigurations[instanceId]
+
+    const labelWidth = getLabelWidth(portTemplate, portConfiguration)
+
+    inputLabelWidths[instanceId] = labelWidth
+  }
+
+  const inputLabelColumnWidth = Math.max(...Object.values(inputLabelWidths), DIMENSIONS.NODE_PORT_MINIMUM_WIDTH)
+
+  const outputLabelWidths: Record<string, number> = {}
+
+  for (const [instanceId, orderIndex] of Object.entries(node.outputs)) {
+    const portTemplate = nodeTemplate.outputs[orderIndex]
+    const portConfiguration = node.portConfigurations[instanceId]
+
+    const labelWidth = getLabelWidth(portTemplate, portConfiguration)
+
+    outputLabelWidths[instanceId] = labelWidth
+  }
+
+  const outputLabelColumnWidth = Math.max(...Object.values(outputLabelWidths), DIMENSIONS.NODE_PORT_MINIMUM_WIDTH)
+
   // Calculate overall width
   const nodeWidth = [
-    DIMENSIONS.NODE_INTERNAL_PADDING, // Input port dot column
-    0, // Input labels column (portConfiguration.label ?? portTemplate.nickname)
-    0, // Input flags column
-    0, // Extra padding between inputs and label,
-    0, // Label
-    0, // Extra padding between outputs and label,
-    0, // Output flags column
-    0, // Output labels column
-    0, // Output port dot column
-  ]
+    DIMENSIONS.NODE_INTERNAL_PADDING,
+    inputLabelColumnWidth,
+    DIMENSIONS.NODE_INTERNAL_PADDING,
+    DIMENSIONS.NODE_LABEL_WIDTH,
+    DIMENSIONS.NODE_INTERNAL_PADDING,
+    outputLabelColumnWidth,
+    DIMENSIONS.NODE_INTERNAL_PADDING,
+  ].reduce((sum, n) => sum + n, 0)
 
   // Calculate overall height
+  const inputPortsHeight = nodeTemplate.inputs.length * DIMENSIONS.NODE_PORT_HEIGHT
+  const outputPortsHeight = nodeTemplate.inputs.length * DIMENSIONS.NODE_PORT_HEIGHT
+  const minimumLabelHeight = nodeTemplate.nickName.length * 12 // not monospace, estimate
 
-  // Calculate port anchors
+  const nodeHeight = Math.max(inputPortsHeight, outputPortsHeight, minimumLabelHeight)
+
+  // Calculate port anchors (center based on height)
 
   // Calculate label axis anchor
 
--- packages/nodes/src/utils/node-dimensions/getNodeHeight.ts
+++ packages/nodes/src/utils/node-dimensions/getNodeHeight.ts
@@ -1,12 +1,12 @@
 import type * as NodePen from '@nodepen/core'
 import { DIMENSIONS } from '@/constants'
 
-const { NODE_INTERNAL_PADDING, NODE_MINIMUM_HEIGHT, NODE_PORT_MINIMUM_HEIGHT } = DIMENSIONS
+const { NODE_INTERNAL_PADDING, NODE_MINIMUM_HEIGHT, NODE_PORT_HEIGHT } = DIMENSIONS
 
 export const getNodeHeight = (template: NodePen.NodeTemplate): number => {
   const { inputs, outputs } = template
 
-  const nodeParameterHeight = Math.max(inputs.length, outputs.length, 1) * NODE_PORT_MINIMUM_HEIGHT
+  const nodeParameterHeight = Math.max(inputs.length, outputs.length, 1) * NODE_PORT_HEIGHT
   const nodeContentHeight = Math.max(nodeParameterHeight, NODE_MINIMUM_HEIGHT)
   const nodeHeight = NODE_INTERNAL_PADDING * 2 + nodeContentHeight
 
