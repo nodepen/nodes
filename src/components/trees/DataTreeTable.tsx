96934bb69c09d35f0cefe270fa921143b1818baf
Chuck Driesler <chuck@nodepen.io>
2026-04-15 14:11:47 +0200

    first draft of panel data tree display

--- src/components/trees/DataTreeTable.tsx
+++ src/components/trees/DataTreeTable.tsx
@@ -9,23 +9,34 @@ type DataTreeTable = {
 export const DataTreeTable = ({ data }: DataTreeTable) => {
     const { apply } = useDispatch()
 
+    console.log({ data })
+
     return (
         <div className='np-w-full np-grid np-grid-cols-[min-content_min-content_1fr] np-relative np-pointer-events-auto'
         >
             {data.branches.sort((a, b) => a.order - b.order).map((branch) => (
                 <>
-                    <div className='np-w-full np-sticky np-top-0 np-p-2 np-bg-light'>
-                        {branch.path}
-                    </div>
                     {branch.values.map((value, i) => (
-                        <>
+                        <div className='np-grid np-grid-cols-subgrid np-col-span-full np-group'>
                             {i === 0
-                                ? <></>
-                                : <div />
+                                ? <div className='np-w-full np-h-8 np-sticky np-top-0 np-mt-1 np-border-l-2 np-border-dark'>
+                                    <div className='np-w-full np-mt-1 np-pl-2 np-pr-2 np-bg-light np-text-sm np-font-semibold np-font-panel np-text-dark'>
+                                        {branch.path}
+                                    </div>
+                                </div>
+                                : <div className='np-w-full np-h-8 np-border-l-2 np-border-dark' />
                             }
-                            <div className='np-p-2'>{value.order}</div>
-                            <div className='np-w-full np-text-right np-p-2 np-whitespace-nowrap'>{value.description}</div>
-                        </>
+                            <div className='np-w-full np-h-full np-pt-1'>
+                                <div className='np-w-full np-h-full np-pl-2 np-pr-2 np-flex np-items-center np-justify-start np-border-l-2 np-border-dark np-text-xs np-font-mono np-text-dark group-hover:np-bg-grey'>
+                                    {value.order}
+                                </div>
+                            </div>
+                            <div className='np-w-full np-h-full np-pt-1 np-overflow-visible'>
+                                <div className='np-w-full np-h-full np-text-right np-pl-2 np-pr-2 np-flex np-items-center np-justify-end np-whitespace-nowrap np-border-r-2 np-border-dark np-text-sm np-font-panel np-text-dark group-hover:np-bg-grey'>
+                                    {value.description}
+                                </div>
+                            </div>
+                        </div>
                     ))}
                 </>
             ))}
