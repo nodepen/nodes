7e0850e205f88f75192a3170afd8012e24d409ad
Chuck Driesler <chuck@nodepen.io>
2026-09-09 11:53:15 +0100

    add type for (unsupported) node toggles

--- src/types/templates/NodeTemplate.ts
+++ src/types/templates/NodeTemplate.ts
@@ -1,16 +1,27 @@
 import type { PortTemplate } from './PortTemplate'
 
 export type NodeTemplate = {
-  guid: string
-  name: string
-  nickName: string
-  description: string
-  keywords: string[]
-  icon?: string
-  libraryName: string
-  category: string
-  subcategory: string
-  isObsolete: boolean
-  inputs: PortTemplate[]
-  outputs: PortTemplate[]
+    guid: string
+    name: string
+    nickName: string
+    description: string
+    keywords: string[]
+    icon?: string
+    libraryName: string
+    category: string
+    subcategory: string
+    isObsolete: boolean
+    inputs: PortTemplate[]
+    outputs: PortTemplate[]
+    toggles: NodeToggle[]
+}
+
+// Bespoke menu items per-component
+// i.e. Cull Duplicates "Leave One" | "Cull All" | "Average"
+export type NodeToggle = {
+    mode: 'checkbox' | 'radio'
+    items: {
+        value: string
+        label: string
+    }[]
 }
